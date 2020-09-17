a940816c67987457145137c126f0befae00ea21a
Andrew Dryneusky <m-lucky@yandex.ru>
2020-09-17 12:43:09 +0300

    111

---
+++ src/by/it/adryneuski/jd01_04/TaskB.java
@@ -0,0 +1,24 @@
+package by.it.adryneuski.jd01_04;
+
+import java.util.Scanner;
+
+public class TaskB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+
+
+        //Scanner sc1 = new Scanner(System.in);
+
+        for (int i = 0; i < n ; i++) {
+
+
+            String s = sc.nextLine();
+
+        System.out.println(s);
+    }
+
+
+        //авттьцкньцнкьцктьertnwrnwr
+}}
---
+++ src/by/it/adryneuski/jd01_04/TaskT.java
@@ -0,0 +1,12 @@
+//package by.it.adryneuski.jd01_04;
+//
+//
+//import static by.it.adryneuski.jd01_04.TaskA.buildOneDimArray;
+//import static by.it.adryneuski.jd01_04.TaskA.printMulTable;
+//
+//public class TaskT {
+//    public static void main(String[] args) {
+//        printMulTable();
+//        buildOneDimArray();
+//    }
+//}
