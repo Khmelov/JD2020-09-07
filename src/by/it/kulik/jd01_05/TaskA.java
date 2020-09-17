6c1866e02781717aeb14a822efc3aaa32caf542d
Artkulik <artkulik@inbox.ru>
2020-09-18 01:25:20 +0300

    01_05

--- src/by/it/kulik/jd01_05/TaskA.java
+++ src/by/it/kulik/jd01_05/TaskA.java
@@ -1,12 +1,32 @@
 package by.it.kulik.jd01_05;
 
+import static java.lang.Math.*;
+
 public class TaskA {
     public static void main(String[] args) {
-        double z, x=0.3, a=756.13;
-        z=Math.cos(Math.pow((x*x+Math.PI/6), 5))-Math.sqrt(x*a*a*a)-Math.log(Math.abs((a-1.12)/4));
-        double c = Math.cos(Math.pow((x*x+Math.PI/6), 5));
-        double s = Math.sqrt(x*a*a*a);
-        double l = Math.log(Math.abs((a-1.12)/4));
-        z=c-s-l;
+        step1();
+        step2();
+        step3();
+
+
+    }
+
+    private static void step1() {
+        double z, x = 0.3, a = 756.13;
+        z = cos(pow((x * x + PI / 6), 5)) - sqrt(x * a * a * a) - log(abs((a - 1.12*x) / 4));
+        double c = cos(pow((x * x + PI / 6), 5));
+        double s = sqrt(x * a * a * a);
+        double l = log(abs((a - 1.12) / 4));
+        double var2=c-s-l;
+         System.out.println(z);
+        System.out.println(var2);
+            }
+    private static void step2() {
+        double a=1.21, b=0.371, y;
+        y= tan(pow((a+b), 2))- cbrt(a+1.5)+a* pow(b, 5)- b/(log(a*a));
+        System.out.println("y="+ y);
+    }
+
+    private static void step3() {
     }
 }
