6c9a59c9fc99d2acc337e95fd964eb0475d26130
Exedin <deathexedin@gmail.com>
2020-09-17 18:32:31 +0300

    jd01_01..04 full done jd01_05 A1-B1 - done jd01_05 TaskB2 - question jd01_05 C1-C2 - full done

---
+++ src/by/it/trukhanovich/jd01_01/TaskC1.java
@@ -0,0 +1,28 @@
+package by.it.trukhanovich.jd01_01;
+
+import java.util.Scanner;
+
+/* Нужно написать программу, которая вводит два числа с клавиатуры
+и выводит их сумму на экран в виде
+
+Ввод (это вы вводите с клавиатуры):
+34 26
+
+Вывод (это должна появится в консоли, обратите внимание на пробелы и слово Sum:
+Sum = 60
+
+*/
+class TaskC1 {
+    public static void main(String[] args) {
+ //       System.out.println("Ввод");
+        Scanner sc1=new Scanner(System.in);
+        int i = sc1.nextInt();
+        int j = sc1.nextInt();
+        int k=i+j;
+        System.out.println("Sum = " + k );
+    }
+
+
+
+
+}
