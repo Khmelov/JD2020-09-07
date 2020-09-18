fe8772d6ea26540637ba807939bdb327f30c57fd
lu55y <mu11er100500@gmail.com>
2020-09-18 18:19:56 +0300

    jd01_01-jd01_04(пару заданий в доработке)

---
+++ src/by/it/yatsevich/jd01_02/TaskC.java
@@ -0,0 +1,26 @@
+package by.it.yatsevich.jd01_02;
+
+public class TaskC {
+ //  public static void main(String[] args) {
+ //      Scanner scanner = new Scanner(System.in);
+ //      int n = scanner.nextInt();
+ //      int arr[][] = new arr[n][n];
+
+//        step1(arr[][]);
+       //      step2();
+       //    step3();
+//    }
+       //  static void step1(int[][] arr){
+       //      for (int i = 0; i <= arr.length-1 ; i++) {
+
+       //       } ()
+
+       //  }
+       //  static void step2(){
+
+       //   }
+       // static void step3(){
+
+       //  }
+
+}
