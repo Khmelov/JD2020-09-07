0da40f424d78cbe761028d995eea67bc2fdf16a8
Andrew Dryneusky <m-lucky@yandex.ru>
2020-09-18 13:47:49 +0300

    118

---
+++ src/by/it/adryneuski/jd01_05/TaskB.java
@@ -0,0 +1,50 @@
+package by.it.adryneuski.jd01_05;
+
+import static java.lang.StrictMath.*;
+
+public class TaskB {
+    public static void main(String[] args) {
+
+        step1();
+        step2();
+    }
+
+    private static void step1()
+        {
+        double y = 0;
+
+        for (double a = 0; a <= 2 ; a+=0.2) {
+            for (int x = 1; x <= 6 ; x++) {
+
+               y = y + pow(7,a) - cos(x);
+
+            }
+            System.out.printf("при a =%6.2f Сумма y = %e",a,y);
+            System.out.println();
+        }
+
+    }
+
+    private static void step2() {
+
+        for (double x = -5.5; x < 2; x += 0.5){
+
+            if ((-2 < x / 2) & (x / 2 <= -1)) {
+                double a = log(abs(sin(x) * sin(x) + 2.74));
+                System.out.printf("при x =%4.2f  a =%-6.2f\n", x, a);
+            } else if ((-1 < x / 2) & (x / 2 < 0.2)) {
+                double a = log(abs(cos(x) * cos(x) + 2.74));
+
+                System.out.printf("при x =%4.2f  a =%-6.2f\n", x, a);
+
+            } else if (x / 2 == 0.2) {
+                double a = log(abs(1 / tan(x) + 2.74));
+                System.out.printf("при x =%4.2f  a =%-6.2f\n", x, a);
+            }
+        System.out.printf("при x =%4.2f  значение не определено\n", x);
+
+    }
+
+    }
+
+}
