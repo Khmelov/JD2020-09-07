1125d3f8a8016cd35a11fd5dc58d02da60ca9ae9
Andrew Dryneusky <m-lucky@yandex.ru>
2020-09-18 14:17:53 +0300

    119

--- src/by/it/adryneuski/jd01_05/TaskB.java
+++ src/by/it/adryneuski/jd01_05/TaskB.java
@@ -6,6 +6,7 @@ public class TaskB {
     public static void main(String[] args) {
 
         step1();
+        System.out.println();
         step2();
     }
 
@@ -29,19 +30,26 @@ public class TaskB {
 
         for (double x = -5.5; x < 2; x += 0.5){
 
-            if ((-2 < x / 2) & (x / 2 <= -1)) {
+            if ((-2 < (x/2))&((x/2) <= -1))
+            {
                 double a = log(abs(sin(x) * sin(x) + 2.74));
-                System.out.printf("при x =%4.2f  a =%-6.2f\n", x, a);
-            } else if ((-1 < x / 2) & (x / 2 < 0.2)) {
+                System.out.printf("при x/2 =%6.2f  a =%-20.10g\n", x/2, a);
+            }
+              else if ((-1 < (x/2))&((x/2) < 0.2))
+              {
                 double a = log(abs(cos(x) * cos(x) + 2.74));
 
-                System.out.printf("при x =%4.2f  a =%-6.2f\n", x, a);
+                System.out.printf("при x/2 =%6.2f  a =%-20.10g\n", x/2, a);
+              }
 
-            } else if (x / 2 == 0.2) {
-                double a = log(abs(1 / tan(x) + 2.74));
-                System.out.printf("при x =%4.2f  a =%-6.2f\n", x, a);
-            }
-        System.out.printf("при x =%4.2f  значение не определено\n", x);
+//              else if (x == 0.4)
+//              {
+//                double a = log(abs(1 / tan(x) + 2.74));
+//
+//                System.out.printf("при x =%4.2f  a =%-6.2f\n", x, a);
+//              }
+            else
+        System.out.printf("при x/2 =%4.2f  значение не определено\n", x/2);
 
     }
 
