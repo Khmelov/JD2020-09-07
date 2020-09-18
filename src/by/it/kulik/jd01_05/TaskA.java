04598f6d2a549b3f6ea6843b39df74c7737d61c7
Artkulik <artkulik@inbox.ru>
2020-09-18 16:45:47 +0300

    jd01_05

--- src/by/it/kulik/jd01_05/TaskA.java
+++ src/by/it/kulik/jd01_05/TaskA.java
@@ -28,5 +28,10 @@ public class TaskA {
     }
 
     private static void step3() {
+        double x=12.1;
+        for(double a=-5; a<=12; a=a+3.75){
+            double f=exp(a*x)-3.45*a;
+            System.out.printf("При а=%6.2f f=%g\n", a, f);
+        }
     }
 }
---
+++ src/by/it/kulik/jd01_05/TaskB.java
@@ -0,0 +1,20 @@
+package by.it.kulik.jd01_05;
+
+import java.util.Scanner;
+
+public class TaskB {
+    public static void main(String[] args) {
+        
+    
+    Scanner scanner=new Scanner(System.in);
+    double x = scanner.nextDouble();
+    double y=0;
+        for(double i=x-1; i<=6; i++){
+        for(double a=0; a<=2; a=a+0.2){
+            y+= Math.pow(7, a) - Math.cos(x);
+            System.out.printf("При а=%6.2f x=%6.2f Сумма y=%g\n", a, x, y);
+        }
+
+    }
+}
+}
