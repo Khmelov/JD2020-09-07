fc0b8eb3876b6fd0bb939efe0e5f7ec00e1c6edc
Exedin <deathexedin@gmail.com>
2020-09-18 10:18:23 +0300

    jd01_01..04 full done jd01_05 - full done

--- src/by/it/trukhanovich/jd01_05/TaskB.java
+++ src/by/it/trukhanovich/jd01_05/TaskB.java
@@ -10,37 +10,34 @@ public class TaskB {
     }
 
     private static void step2() {
-        double x = (Math.random() * (8) - 6);
-        if (x == -6) x = x + Double.MIN_VALUE;
-        do {
-            System.out.printf("double x=%2.1f\n", x);
-//        for (double x = -5.5; x < 2; x = x + 0.5) {
-//                if (x <= -6) continue;
-            if (x / 2 > -2 && x / 2 <= -1) {
+        System.out.println("Step2");
+        for (double x = -5.5; x < 2; x = x + 0.5) {
+                if (x / 2 > -2 && x / 2 <= -1) {
                 double b = sin(x * x);
-                double a = log(abs(b + 2.74));
+                double a = log10(abs(b + 2.74));
                 System.out.printf("При x/2=%f; a=%e\n", x / 2, a);
             }
             if (x / 2 > -1 && x / 2 <= 0.2) {
                 double b = cos(x * x);
-                double a = log(abs(b + 2.74));
+                double a = log10(abs(b + 2.74));
                 System.out.printf("При x/2=%f; a=%e\n", x / 2, a);
             }
             if (x / 2 == 0.2) {
                 double b = cos(x * x);
-                double a = log(abs(b + 2.74));
+                double a = log10(abs(b + 2.74));
                 System.out.printf("При x/2=%f; a=%e\n", x / 2, a);
             }
             if ((x / 2 <= -2) || (x / 2 > 0.2)) {
                 System.out.printf("При x/2=%f; вычисления не определены\n", x / 2);
             }
-            x=x+0.5;
-        } while (x<2);
+        }
+
     }
 
 
 
         private static void step1 () {
+            System.out.println("Step1");
             for (double a = 0; a <= 2; a = a + 0.2) {
                 double y = 0;
                 for (int x = 1; x <= 6; x++) {
