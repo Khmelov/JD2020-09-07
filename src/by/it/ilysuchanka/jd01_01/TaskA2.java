b7f58b5084816626ecef9edddf5d9a941de3e487
nosorog7798 <perec2522559vlad@gmail.com>
2020-09-21 00:59:31 +0300

    v

--- src/by/it/ilysuchanka/jd01_01/TaskA2.java
+++ src/by/it/ilysuchanka/jd01_01/TaskA2.java
@@ -9,5 +9,11 @@ package by.it.ilysuchanka.jd01_01;
 */
 
 class TaskA2 {
-
+    public static void main(String[] args) {
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+    }
 }
