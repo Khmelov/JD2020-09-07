d032c20e06446146e47eef8d13dfa765e5d776e6
Artkulik <artkulik@inbox.ru>
2020-09-20 23:28:32 +0300

    01_06TaskB

--- src/by/it/kulik/jd01_05/TaskB.java
+++ src/by/it/kulik/jd01_05/TaskB.java
@@ -1,23 +1,19 @@
 package by.it.kulik.jd01_05;
 
-import java.util.Scanner;
+
 
 public class TaskB {
     public static void main(String[] args) {
-        
-    
-    Scanner scanner=new Scanner(System.in);
-    double x = scanner.nextDouble();
-    double y=0;
-        for(double i=x-1; i<=6; i++){
-            double a=0;
-            while (a<=2) {
-                a = a + 0.2;
-                y=y+Math.pow(7, a) - Math.cos(x);
-                System.out.printf("При а=%6.2f x=%6.2f Сумма y=%g\n", a, x, y);
+        double y;
 
+        for (int x = 0; x < 7; x++) {
+            for (double a = 0; a <= 2 ; a=a+0.2) {
+                y=Math.pow(7, a) - Math.cos(x);
+                System.out.printf("При а=%6.2f Сумма y=%g\n", a, y);
             }
 
         }
-}
-}
+            }
+
+        }
+
--- src/by/it/kulik/jd01_06/TaskB1.java
+++ src/by/it/kulik/jd01_06/TaskB1.java
@@ -1,20 +1,17 @@
 package by.it.kulik.jd01_06;
 
 public class TaskB1 {
-
+private static String[] glasnye= {"А,а,и,И,У,у,Е,е,э,Э,О,о,Ю,ю,Я,я"};
+    private static String[] soglasnye= {"Й,й,ц,Ц,к,К,Н,н,Г,г,Ш,ш,Щ,щ,З,з,Х,х," +
+            "Ф,ф,в,В,П,п,Р,р,Л,л,Д,д,Ж,ж,ч,Ч,С,с,М,м,Т,т,Ь,ь,Б,б"};
     public static void main(String[] args) {
-        String letters = "ЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБйцкнгшщзхъфвпрлджчсмтьб";
+        String letters = "Й Ц К Н Г Ш Щ З Х Ъ Ф В П Р Л Д Ж Ч С М Т Ь Б й " +
+                "ц к н г ш щ з х ъ ф в п р л д ж ч с м т ь б";
         String letters2 = "ЁёУуЕеЫыАаОоЯяИиЮюЭэ";
         String text = Poem.text;
         text = text.trim();
-        for (String word : text.split(" ")) {
-            if (letters.contains(word.substring(0, 1)))
-                {
+        String[] words = text.split(" ");
 
-                    System.out.println(word);
-                }
-
-        }
 
     }
 }
