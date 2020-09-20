e4b5a8415cae6a27e789f4677b966e3a72813cf5
nikitahrynts <hrynts2002@gmail.com>
2020-09-20 03:05:27 +0300

    jd01_05 started

--- src/by/it/hryntsaliou/jd01_05/TaskC.java
+++ src/by/it/hryntsaliou/jd01_05/TaskC.java
@@ -5,17 +5,55 @@ import static java.lang.StrictMath.*;
 
 class TaskC {
     public static void main(String[] args) {
-        step1();
-        //step2();
+        //step1();
+        step2();
+    }
+
+    private static void step2() {
+        double[] array = new double[30];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (random() * 348 + 103);
+        }
+        outBegin(array, 5);
+        outResult(array, 2);
+        //╔  ═ ║  ╗ ╚ ╝ ╣  ╦ ╩  ╬ ║
+    }
+
+    private static void outBegin(double[] array, int colCount) {
+        int rows = 0;
+        System.out.println("Massiv M <index to rows>");
+        System.out.println("╔═══════════╦═══════════╦═══════════╦═══════════╦═══════════╗");
+        for (int i = 1; i < array.length; i++) {
+            System.out.printf("║M[%2d]=%4.1f", i, array[i]);
+            if (i % colCount == 0) {
+                rows++;
+                if ((array.length - 1) % colCount == 0 && i == array.length - 1) {
+                    System.out.println("║");
+                    System.out.println("╚═══════════╩═══════════╩═══════════╩═══════════╩═══════════╝");
+                    return;
+                }
+                System.out.println("║");
+                System.out.println("╠═══════════╬═══════════╬═══════════╬═══════════╬═══════════╣");
+            }
+        }
+        for (int j = 0; j < (rows + 1) * colCount - (array.length - 1); j++) {
+            System.out.print("║           ");
+        }
+        System.out.println("║");
+        System.out.println("╚═══════════╩═══════════╩═══════════╩═══════════╩═══════════╝");
+    }
+
+    private static void outResult(double[] array, int colCount) {
+
     }
 
     private static void step1() {
-        double[] array = new double[(int)(random()*21+20)];
+        double[] array = new double[(int) (random() * 21 + 20)];
         int i = 0;
-        for (double x = 5.33; x <= 9; x += (9-5.33)/ array.length) {
+        for (double x = 5.33; x <= 9; x += (9 - 5.33) / array.length) {
             double z = cbrt(x * x + 4.5);
-            if(i<array.length) {
-                array[i]=z;
+            if (i < array.length) {
+                array[i] = z;
                 i++;
             }
         }
@@ -35,14 +73,15 @@ class TaskC {
         int count = 0;
         double sum = 1;
         System.out.println();
-        System.out.print("Массив B[] из элементов массива A > 3.5 ");
+        System.out.println("Массив B[] из элементов массива A > 3.5 ");
         for (int i = 0; i < array.length; i++) {
             if (array[i] > 3.5) {
                 System.out.printf("B[ %d ] = %5.4f  ", count, array[i]);
                 count++;
                 sum *= array[i];
+                if (count % colCount == 0 || count % array.length == 0) System.out.println();
             }
-            if (count % colCount == 0 || count % array.length == 0) System.out.println();
+
         }
         double srGeom = pow(sum, (double) 1 / count);
         System.out.println();
---
+++ src/by/it/hryntsaliou/jd01_06/TaskA2.java
@@ -0,0 +1,6 @@
+package by.it.hryntsaliou.jd01_06;
+
+class TaskA2 {
+    public static void main(String[] args) {
+    }
+}
