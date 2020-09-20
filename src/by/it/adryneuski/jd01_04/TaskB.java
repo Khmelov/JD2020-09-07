9bea7fb1c05799dd0f23ecfdf5cfd04002507b40
MrLucky74 <746239qw>
2020-09-20 12:54:10 +0300

    25

--- src/by/it/adryneuski/jd01_04/TaskB.java
+++ src/by/it/adryneuski/jd01_04/TaskB.java
@@ -4,21 +4,29 @@ import java.util.Scanner;
 
 public class TaskB {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(n);
 
 
-        //Scanner sc1 = new Scanner(System.in);
+        String[] name = new String[n];
+        Scanner sc1 = new Scanner(System.in);
 
-        for (int i = 0; i < n ; i++) {
+        for (int i = 0; i < n; i++)
+        {
+            name[i] = sc1.nextLine();
+
+        }
 
 
-            String s = sc.nextLine();
 
-        System.out.println(s);
+        for (int i = 0; i < n; i++)
+        {
+            System.out.print (" " + name[i]);
+        }
+        System.out.println();
     }
 
 
+  }
 
-}}
