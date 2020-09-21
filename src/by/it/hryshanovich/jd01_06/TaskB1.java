2200584d48f6ffc8e7838263062a80a80b58bb19
Hryshanovich <alexandars6236@gmail.com>
2020-09-21 18:50:59 +0300

    jd01_01...05 start 06B

---
+++ src/by/it/hryshanovich/jd01_06/TaskB1.java
@@ -0,0 +1,16 @@
+package by.it.hryshanovich.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder(Poem.text);
+        Pattern pattern=Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher= pattern.matcher(Poem.text);
+        while (matcher.find()){
+            String word = matcher.group();
+            if (cheskWord(word))
+        }
+    }
+}
