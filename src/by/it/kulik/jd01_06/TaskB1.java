aa02c05fdc49af0d5a94bdf693bc89135277505e
Artkulik <artkulik@inbox.ru>
2020-09-21 21:37:10 +0300

    b10106

--- src/by/it/kulik/jd01_06/TaskB1.java
+++ src/by/it/kulik/jd01_06/TaskB1.java
@@ -1,17 +1,18 @@
 package by.it.kulik.jd01_06;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class TaskB1 {
-private static String[] glasnye= {"А,а,и,И,У,у,Е,е,э,Э,О,о,Ю,ю,Я,я"};
-    private static String[] soglasnye= {"Й,й,ц,Ц,к,К,Н,н,Г,г,Ш,ш,Щ,щ,З,з,Х,х," +
-            "Ф,ф,в,В,П,п,Р,р,Л,л,Д,д,Ж,ж,ч,Ч,С,с,М,м,Т,т,Ь,ь,Б,б"};
+
+private static final String G= "АаиИУуЕеэЭОоЮюЯя";
+
     public static void main(String[] args) {
-        String letters = "Й Ц К Н Г Ш Щ З Х Ъ Ф В П Р Л Д Ж Ч С М Т Ь Б й " +
-                "ц к н г ш щ з х ъ ф в п р л д ж ч с м т ь б";
-        String letters2 = "ЁёУуЕеЫыАаОоЯяИиЮюЭэ";
-        String text = Poem.text;
-        text = text.trim();
-        String[] words = text.split(" ");
 
 
     }
+    static boolean checkWord(String word){
+        return  G.indexOf(word.charAt(0))<0 && G.indexOf(word.charAt(word.length()-1))>=0;
+    }
+
+
 }
