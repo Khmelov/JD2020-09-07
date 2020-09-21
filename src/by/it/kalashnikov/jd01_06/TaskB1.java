1c9fcb9806f4bf82c4fd22bd3f4b2c6921c6f17e
KalashnikovKV <kirillrik1994@gmail.com>
2020-09-21 16:46:58 +0300

    jd01_06 start TaskB1

---
+++ src/by/it/kalashnikov/jd01_06/TaskB1.java
@@ -0,0 +1,24 @@
+package by.it.kalashnikov.jd01_06;
+
+
+class TaskB1 {
+    private static final String [] gl = { "а, е, ё, и, о, у, ы, э, ю, я"};
+
+//    Приватный метод без regex для проверки условия :
+//    начинается ли слово с согласной буквы и заканчивается ли на гласную
+    private static void s (String word){
+
+
+
+    }
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder( Poem.text);
+
+
+
+ //       текст Poem в массив строк sb
+
+    }
+//    вывести слова в консоль начинающиеся с согласной и заканчивающиеся на гласную
+}
