851336a0d7af1af15bf6b9805dec298985f513c3
Exedin <deathexedin@gmail.com>
2020-09-22 01:45:54 +0300

    jd01_07 C1 started

---
+++ src/by/it/trukhanovich/jd01_07/Runner.java
@@ -0,0 +1,17 @@
+package by.it.trukhanovich.jd01_07;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+        Var v1= new Scalar(3.1415);
+        Var v2 = new Scalar("3.1415");
+        System.out.println(v1);
+        System.out.println(v2);
+        Var v3=new Vector(new double[]{1.0, 2.0, 4.0});
+        Var v4= new Vector (new String("{1.1, 2.1, 4.1}"));
+        System.out.println(v3);
+        System.out.println(v4);
+
+    }
+}
---
+++ src/by/it/trukhanovich/jd01_07/Vector.java
@@ -0,0 +1,41 @@
+package by.it.trukhanovich.jd01_07;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Vector extends Var {
+
+  private double [] value;
+
+  public Vector(double[] value) {
+   this.value = value;
+  }
+ public Vector(Vector vector) {
+  this.value = vector.value;
+ }
+ public Vector (String strVector) {
+        StringBuilder sb= new StringBuilder(strVector);
+        sb.deleteCharAt(0);
+        sb.deleteCharAt(sb.length()-1);
+     strVector=sb.toString();
+     String []arrayString=strVector.split(",");
+
+  this.value= new double[arrayString.length];
+    for (int i = 0; i < arrayString.length; i++) {
+  value[i]=Double.parseDouble(arrayString[i]);
+  }
+ }
+
+ @Override
+ public String toString() {
+   StringBuilder sb= new StringBuilder("{");
+   String delimiter= "";
+  for (double v : value) {
+   sb.append(delimiter).append(v);
+   delimiter=", ";
+   }
+  sb.append("}");
+  return sb.toString();
+ }
+}
