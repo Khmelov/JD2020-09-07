8978178a88d16d11fe88f260b31e70967b8470d8
Andrew <m-lucky@yandex.ru>
2020-09-22 22:47:38 +0400

    38

---
+++ src/by/it/adryneuski/jd01_07/Runner.java
@@ -0,0 +1,4 @@
+package by.it.adryneuski.jd01_07;
+
+public class Runner {
+}
---
+++ src/by/it/adryneuski/jd01_07/TaskA1.java
@@ -0,0 +1,35 @@
+package by.it.adryneuski.jd01_07;
+
+public class TaskA1 {
+    public static void main(String[] args) {
+
+
+    }
+}
+    public abstract class Var {
+
+
+        }
+
+    class Scalar extends Var
+    {
+        private double val = 3.1415;
+        public double GetVal()
+        {return val;}
+
+    public Scalar(double value)
+        {
+         this.val = value;
+
+        }
+    public Scalar()
+        {
+
+        }
+    public Scalar()
+        {
+
+        }
+    }
+
+
