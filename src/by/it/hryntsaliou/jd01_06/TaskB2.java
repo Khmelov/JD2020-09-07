ca3db2be87a7377f7893337d9f3878f4dc1967c8
nikitahrynts <hrynts2002@gmail.com>
2020-09-24 22:21:24 +0300

    jd01_07 started

--- src/by/it/hryntsaliou/jd01_06/TaskB2.java
+++ src/by/it/hryntsaliou/jd01_06/TaskB2.java
@@ -1,63 +1,81 @@
 package by.it.hryntsaliou.jd01_06;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class TaskB2 {
     public static void main(String[] args) {
         String text = Poem.text;
+        text = deleteShifts(text);
+        text = delete2points(text);
+        String[] lines = getStrings(text);
+        spacesDeletion(lines);
+        linesSort(lines);
+        for (String line : lines) {
+            System.out.println(line.trim());
+        }
+    }
+
+    private static String deleteShifts(String text) {
         Pattern pattern = Pattern.compile("[\\n]");
         Matcher matcher = pattern.matcher(text);
         while(matcher.find()) {
             text = matcher.replaceAll(" ");
         }
-        Pattern pattern3 = Pattern.compile("[. ]{4}[а-я]");
-        Matcher matcher3 = pattern3.matcher(text);
-        while (matcher3.find()) {
-            text = matcher3.replaceAll("");
+        return text;
+    }
+
+    private static String delete2points(String text) {
+        Pattern pattern = Pattern.compile("[. ]{4}[а-я]");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            text = matcher.replaceAll("");
         }
-        String[] lines = text.split("[\\.]");
-        Pattern pattern1 = Pattern.compile("[\\p{Punct}]");
+        return text;
+    }
+
+    private static String[] getStrings(String text) {
+        String[] lines = text.split("[.]");
+        Pattern pattern = Pattern.compile("[\\p{Punct}]");
         for (int i = 0; i < lines.length; i++) {
-            Matcher matcher1 = pattern1.matcher(lines[i]);
-            while (matcher1.find()) {
+            Matcher matcher = pattern.matcher(lines[i]);
+            while (matcher.find()) {
                 for (int j = 0; j < lines.length; j++) {
-                    lines[i] = matcher1.replaceAll(" ");
+                    lines[i] = matcher.replaceAll(" ");
                 }
             }
         }
+        return lines;
+    }
 
-        Pattern pattern2 = Pattern.compile(" {2,}");
-        for (int i = 0; i < lines.length; i++) {
-            Matcher matcher2 = pattern2.matcher(lines[i]);
-            while (matcher2.find()) {
-                for (int j = 0; j < lines.length; j++) {
-                    lines[i] = matcher2.replaceAll(" ");
-                }
-            }
-        }
-
-        int b = lines.length-1;
-        String buf=new String();
-        boolean det;
+    private static void linesSort(String[] lines) {
+        int length = lines.length-1;
+        String line;
+        boolean ok;
         do {
-            det=false;
-            for (int i = 0; i < b; i++) {
+            ok=false;
+            for (int i = 0; i < length; i++) {
                 if (lines[i].length()>0 && lines[i].length() > lines[i + 1].length()) {
-                    buf = lines[i + 1];
+                    line = lines[i + 1];
                     lines[i + 1] = lines[i];
-                    lines[i] = buf;
-                    det = true;
+                    lines[i] = line;
+                    ok = true;
                 }
             }
-            b--;
+            length--;
         }
-        while (det);
+        while (ok);
+    }
 
+    private static void spacesDeletion(String[] lines) {
+        Pattern pattern = Pattern.compile(" {2,}");
         for (int i = 0; i < lines.length; i++) {
-            System.out.println(lines[i].trim());
-
+            Matcher matcher = pattern.matcher(lines[i]);
+            while (matcher.find()) {
+                for (int j = 0; j < lines.length; j++) {
+                    lines[i] = matcher.replaceAll(" ");
+                }
+            }
         }
     }
 }
