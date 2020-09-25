09484f1ddb4c5240447283303813aa83aa82e96f
Andrew Dryneusky <m-lucky@yandex.ru>
2020-09-25 14:02:15 +0300

    42

--- src/by/it/adryneuski/jd01_06/TST.java
+++ src/by/it/adryneuski/jd01_06/TST.java
@@ -5,11 +5,12 @@ import java.util.regex.Pattern;
 
 public class TST {
     public static void main(String[] args) {
+        String input = "Hello Java! Hello JavaScript! JavaSE 8";
+        Pattern pattern = Pattern.compile("Java(\\w*)");
+        Matcher matcher = pattern.matcher(input);
+        boolean found = matcher.matches();
+        while(matcher.find())
+            System.out.println(matcher.group());
 
-        String text = "FIFA will never regret it";
-        String[] words = text.split(" ");
-        for(String word : words){
-            System.out.println(word);
-        }
     }
 }
