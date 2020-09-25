c58c2fbd759deee778966abd3dabf84f582e38ae
Exedin <deathexedin@gmail.com>
2020-09-26 01:30:59 +0300

    jd01_08 Matrix started

--- src/by/it/trukhanovich/jd01_08/Matrix.java
+++ src/by/it/trukhanovich/jd01_08/Matrix.java
@@ -7,6 +7,9 @@ import java.util.regex.Pattern;
 
 class Matrix extends Var {
      private double [][] value;
+    public double[][] getValue() {
+        return value;
+    }
 
     public Matrix(double[][] value) {
         this.value = Arrays.copyOf(value, value.length);
@@ -28,6 +31,9 @@ class Matrix extends Var {
         this.value= matrix;
         getArrayStrigToDouble(sb,matrix);
     }
+
+
+
     private void getArrayStrigToDouble(StringBuilder sb, double[][] matrix)
     {
         Pattern p3= Pattern.compile("(\\d+\\.\\d+)|(\\d+)");
@@ -56,6 +62,56 @@ class Matrix extends Var {
         return i;
     }
 
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Matrix){
+            this.value=value;
+            double [][] s1=((Matrix) other).getValue();
+            double[][] result=new double[value.length][value[0].length];
+            for (int i = 0; i < result.length; i++) {
+                for (int j = 0; j < result[i].length; j++) {
+                    result [i][j]=value[i][j]+s1[i][j];
+                }
+            }
+            return new Matrix(result);
+        }
+        return super.add(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar) {
+            double[][] res = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < res.length; i++) {
+                for (int j = 0; j < res[i].length; j++) {
+                    res[i][j] = value[i][j] * ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(res);
+        } else if (other instanceof Vector) {
+            double result[] = new double[value.length];
+            double[] vetc = Arrays.copyOf(((Vector) other).getValue(), ((Vector) other).getValue().length);
+            for (int i = 0; i < value.length; i++) {
+                for (int j = 0; j < value[i].length; j++) {
+                    result[i] = result[i] + value[i][j] * vetc[j];
+                }
+            }
+            return new Vector(result);
+        }
+        else  {
+            double[][] matrixLeft = this.value;
+            double[][] matrixRight = ((Matrix) other).getValue();
+            double[][] result = new double[matrixLeft.length][matrixRight[0].length];
+            for (int i = 0; i < matrixLeft.length; i++) {
+                for (int j = 0; j < matrixRight[0].length; j++) {
+                    for (int k = 0; k < matrixRight.length; k++) {
+                        result[i][j] = result[i][j] + matrixLeft[i][k] * matrixRight[k][j];
+                    }
+                }
+            }
+            return new Matrix(result);
+        }
+    }
 
     @Override
     public String toString() {
