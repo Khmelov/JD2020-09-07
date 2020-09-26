fd5edfa9e9e8ea394fa8ccb636f20e1285248480
emakovski <egorushka.man@gmail.com>
2020-09-26 09:10:20 +0300

    jd01-jd07

--- src/by/it/makovsky/jd01_07/Vector.java
+++ src/by/it/makovsky/jd01_07/Vector.java
@@ -1,11 +1,14 @@
 package by.it.makovsky.jd01_07;
 
 class Vector extends Var{
-
+    private double [] value;
+    Vector (double [] value){
+        this.value=value;
+    }
 
 
     @Override
     public String toString() {
-        return " ";
+        return (value.toString());
     }
 }
