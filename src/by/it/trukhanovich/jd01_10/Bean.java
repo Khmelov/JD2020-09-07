cbb3b62e9d5a46c95856f55c4ed41693df142667
Exedin <deathexedin@gmail.com>
2020-10-01 01:44:45 +0300

    jd01_10 full done

--- src/by/it/trukhanovich/jd01_10/Bean.java
+++ src/by/it/trukhanovich/jd01_10/Bean.java
@@ -1,20 +1,21 @@
 package by.it.trukhanovich.jd01_10;
 
 public class Bean {
-        @Param(a=2,b=5)
+        @Param(a=22,b=5)
         double sum (int a, int b) {
             return a+b;
         }
-        @Param(a=20,b=50)
+        @Param(a=25,b=50)
         double max (int a, int b) {
             return a>b?a:b;
         }
-        @Param(a=22,b=55)
-        static double min (int a, int b) {
-            return a<b?a:b;
-        }
-        static double avg (int a, int b) {
+    @Param(a=22,b=55)
+    static double avg (int a, int b) {
             return (a+b)/2.0;
         }
+//    @Param(a=22,b=55)
+    static double min (int a, int b) {
+        return a<b?a:b;
+    }
 
 }
