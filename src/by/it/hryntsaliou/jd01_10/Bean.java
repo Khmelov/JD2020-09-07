8ae30e961c3932f5f5d4d83ea40cc820890b43b9
nikitahrynts <hrynts2002@gmail.com>
2020-10-02 00:58:41 +0300

    jd01_10

--- src/by/it/hryntsaliou/jd01_10/Bean.java
+++ src/by/it/hryntsaliou/jd01_10/Bean.java
@@ -4,18 +4,21 @@ class Bean {
 
     @Param(a = 1, b = 2)
     static double sum(int a, int b) {
-        return a+b;
+        return a + b;
     }
+
     @Param(a = 3, b = 4)
     static double max(int a, int b) {
         return a > b ? a : b;
     }
+
     @Param(a = 5, b = 6)
     double min(int a, int b) {
         return a < b ? a : b;
     }
+
     double avg(int a, int b) {
-        return (a+b) / 2.0;
+        return (a + b) / 2.0;
     }
 
 }
