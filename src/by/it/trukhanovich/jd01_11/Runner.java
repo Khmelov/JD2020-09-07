f81b7a20a8b90f076eb478dcbb4b1a0aee1ebee1
Exedin <deathexedin@gmail.com>
2020-10-02 23:32:25 +0300

    jd01_11 taskA started

---
+++ src/by/it/trukhanovich/jd01_11/Runner.java
@@ -0,0 +1,24 @@
+package by.it.trukhanovich.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> listOk = new ArrayList<String>();
+        listOk.add("one");
+        listOk.add("2");
+        listOk.add("3");
+        listOk.add("4");
+        listOk.add("5");
+        listOk.add("6");
+        System.out.println(listOk);
+        System.out.println(listOk.size());
+        listOk.remove(2);
+        System.out.println(listOk);
+        System.out.println(listOk.size());
+
+
+
+    }
+}
