145c377649ce45e8c7e1da67aa3ccb26bc7b7a59
lu55y <mu11er100500@gmail.com>
2020-10-04 02:46:21 +0300

    jd01_10 full

--- src/by/it/yatsevich/jd01_10/Param.java
+++ src/by/it/yatsevich/jd01_10/Param.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 
-public @interface Param {
+@interface Param {
     int a();
     int b();
 }
--- src/by/it/yatsevich/jd01_10/PrintMath.java
+++ src/by/it/yatsevich/jd01_10/PrintMath.java
@@ -1,30 +1,51 @@
 package by.it.yatsevich.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
-public class PrintMath {
+class PrintMath {
 
     public static void main(String[] args) {
         Class<Math> structureMathClass = Math.class;
-        Method[] methods = structureMathClass.getDeclaredMethods();
-        for (Method method: methods) {
-            int modifiers = method.getModifiers();
+        Field[] fields = structureMathClass.getFields();
+        Method[] methods = structureMathClass.getMethods();
+
+        for (Field field : fields) {
             StringBuilder out = new StringBuilder();
-            String delimetr="";
+            String delimetr = "";
+            int modifiers = field.getModifiers();
             if (Modifier.isPublic(modifiers)){
                 out.append("public ");
                 if (Modifier.isStatic(modifiers)){
+                    out.append("static ");
+                    Class<?> type = field.getType();
+                    out.append(type.getSimpleName());
+                    out.append(' ');
+                    out.append(field.getName());
+                    out.append(delimetr);
+                }
+            }System.out.println(out);
+        }
+
+        for (Method method : methods) {
+            StringBuilder out = new StringBuilder();
+            String delimetr = "";
+            int modifiers = method.getModifiers();
+            if (Modifier.isPublic(modifiers)) {
+                out.append("public ");
+                if (Modifier.isStatic(modifiers)) {
                     out.append("static ");
                     Class<?> returnType = method.getReturnType();
                     out.append(returnType.getSimpleName());
                     out.append(' ');
                     out.append(method.getName());
                     out.append("(");
-                    for (Class<?> parametrType: method.getParameterTypes()) {
+                    for (Class<?> parametrType : method.getParameterTypes()) {
                         out.append(delimetr).append(parametrType);
-                        delimetr=",";
-                    }out.append(")");
+                        delimetr = ",";
+                    }
+                    out.append(")");
                     System.out.println(out);
                 }
             }
