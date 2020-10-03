ea4c6cf9a6b6b8faf7715a75891bad05c2c77dea
Exedin <deathexedin@gmail.com>
2020-10-03 03:15:44 +0300

    jd01_11 taskC in progress

--- src/by/it/trukhanovich/jd01_11/Runner.java
+++ src/by/it/trukhanovich/jd01_11/Runner.java
@@ -1,55 +1,66 @@
 package by.it.trukhanovich.jd01_11;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Runner {
     public static void main(String[] args) {
-        List<String> listOk = new ArrayList<String>();
-        listOk.add("one");
-        listOk.add("2");
-        listOk.add("3");
-        listOk.add("4");
-        listOk.add("5");
-        listOk.add("6");
-        System.out.println(listOk);
-        System.out.println(listOk.size());
-        listOk.remove(2);
-        System.out.println(listOk);
-        System.out.println(listOk.size());
-        listOk.set(2,"new string");
-        System.out.println(listOk);
-        listOk.add(3,"123");
-        System.out.println(listOk);
-        listOk.addAll(listOk);
-        System.out.println(listOk);
-//        ArrayList ggg=new ArrayList();
-//        ggg.toArray()
+        Set<String> strings = new HashSet<String>();
+        strings.add("one");
+        strings.add("one");
+        strings.add("1");
+        strings.add("2");
+        strings.add("3");
+        strings.add("4");
+        strings.add("124");
+        System.out.println(strings.toString());
 
 
-        //my realization
-        System.out.println("моя реализация");
-        ListB<Object> myListOk = new ListB<>();
-        myListOk.add("myOne");
-        myListOk.add("my1");
-        myListOk.add("my2");
-        myListOk.add("my3");
-        myListOk.add("my4");
-        myListOk.add("my5");
-        myListOk.add("my6");
-        System.out.println(myListOk);
-        System.out.println(myListOk.size());
-        System.out.println(myListOk.get(4));
-        myListOk.set(2,"myNewString");
-        System.out.println(myListOk);
-        myListOk.add(3,"new");
-        System.out.println(myListOk);
-        System.out.println("addAll");
-        System.out.println("Размер массива:"+myListOk.size());
-        myListOk.addAll(myListOk);
-        System.out.println(myListOk);
-        System.out.println("Размер массива:"+myListOk.size());
+
+
+//        List<String> listOk = new ArrayList<String>();
+//        listOk.add("one");
+//        listOk.add("2");
+//        listOk.add("3");
+//        listOk.add("4");
+//        listOk.add("5");
+//        listOk.add("6");
+//        System.out.println(listOk);
+//        System.out.println(listOk.size());
+//        listOk.remove(2);
+//        System.out.println(listOk);
+//        System.out.println(listOk.size());
+//        listOk.set(2,"new string");
+//        System.out.println(listOk);
+//        listOk.add(3,"123");
+//        System.out.println(listOk);
+//        listOk.addAll(listOk);
+//        System.out.println(listOk);
+////        ArrayList ggg=new ArrayList();
+////        ggg.toArray()
+//
+//
+//        //my realization
+//        System.out.println("моя реализация");
+//        ListB<Object> myListOk = new ListB<>();
+//        myListOk.add("myOne");
+//        myListOk.add("my1");
+//        myListOk.add("my2");
+//        myListOk.add("my3");
+//        myListOk.add("my4");
+//        myListOk.add("my5");
+//        myListOk.add("my6");
+//        System.out.println(myListOk);
+//        System.out.println(myListOk.size());
+//        System.out.println(myListOk.get(4));
+//        myListOk.set(2,"myNewString");
+//        System.out.println(myListOk);
+//        myListOk.add(3,"new");
+//        System.out.println(myListOk);
+//        System.out.println("addAll");
+//        System.out.println("Размер массива:"+myListOk.size());
+//        myListOk.addAll(myListOk);
+//        System.out.println(myListOk);
+//        System.out.println("Размер массива:"+myListOk.size());
 
 
 
---
+++ src/by/it/trukhanovich/jd01_11/SetC.java
@@ -0,0 +1,99 @@
+package by.it.trukhanovich.jd01_11;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+public class SetC<T> implements Set<T> {
+    private T[] elements= (T[]) new Object[]{};
+
+    private int size=0;
+
+    @Override
+    public boolean add(T t) {
+        if (size==elements.length)
+        {
+            elements=Arrays.copyOf(elements,elements.length+1);
+        }
+        elements[size++]=t;
+        return true;
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder out=new StringBuilder();
+        out.append("[");
+        for (int i = 0; i < size; i++) {
+            if (i>0) {
+                out.append(", ");
+            }
+            out.append(elements[i]);
+        }
+        out.append("]");
+        return out.toString();
+    }
+    //no impl
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
