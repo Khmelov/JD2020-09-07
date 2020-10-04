4b73c9fab01f99efe5e6058a5574f198f676dd9e
Kirill <kstbox17@gmail.com>
2020-10-05 00:56:29 +0300

    jd1_05

---
+++ src/by/it/stefankin/jd01_10/PrintMath.java
@@ -0,0 +1,40 @@
+package by.it.stefankin.jd01_10;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class PrintMath {
+    public static void main(String[] args) {
+        Class<Math> c = Math.class;
+        Method[] met=c.getDeclaredMethods();
+        for (Method method : met) {
+            int modifiers = method.getModifiers();
+            StringBuilder out = new StringBuilder();
+            if (Modifier.isPublic(modifiers)) {
+                out.append("public ");
+                if (Modifier.isStatic(modifiers))
+                    out.append("static ");
+                Class<?> returnType = method.getReturnType();
+                out.append(returnType.getSimpleName());
+                out.append(" ");
+                out.append(method.getName());
+                out.append('(');
+
+                Class<?>[] parameterType = method.getParameterTypes();
+
+                for (int g=0;g<method.getParameterTypes().length;g++) {
+
+                    out.append(parameterType[g].getSimpleName());
+                    if (method.getParameterTypes().length>1 && g!=method.getParameterTypes().length-1)
+                            {out.append(",");}
+
+                }
+                out.append(')');
+                System.out.println(out);
+            }
+        }
+
+
+
+    }
+}
