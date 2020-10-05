b00fbaed035d4235996f580cfe2e4dbf0f2ce2d5
IvanShkatula <Iwan_Shkatula@tut.by>
2020-10-05 18:29:15 +0300

    jd01_11B

--- src/by/it/shkatula/jd01_10/PrintMath.java
+++ src/by/it/shkatula/jd01_10/PrintMath.java
@@ -2,16 +2,41 @@ package by.it.shkatula.jd01_10;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
+import java.security.Signature;
+import java.util.jar.Attributes;
 
 public class PrintMath {
     public static void main(String[] args) {
-        Class<Math> mathStructure = Math.class;
-        Method[] methods = mathStructure.getDeclaredMethods();
+        Class<?> structMathClass = Math.class;
+        Method[] methods = structMathClass.getDeclaredMethods();
         for (Method method : methods) {
-            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
-                if ((method.getModifiers() & Modifier.STATIC) == Modifier.STATIC)
- //                   if ((method.getModifiers() & Modifier) == Modifier.STATIC)
-                    System.out.println(method);
+            int modifiers = method.getModifiers();
+            StringBuilder out = new StringBuilder();
+            if (Modifier.isPublic(modifiers)) {
+                out.append("public ");
+                if (Modifier.isStatic(modifiers)) {
+                    out.append("static ");
+                    String returnType = method.getReturnType().getSimpleName();
+                    if (returnType.equalsIgnoreCase("float") || returnType.equalsIgnoreCase("double")) {
+
+                        out.append(returnType);
+
+                        out.append(' ');
+                        out.append(method.getName());
+                        if (method.getName().equalsIgnoreCase("abs") || method.getName().equalsIgnoreCase("pow"))
+                        out.append('(');
+                        for (Class<?> parameterType : method.getParameterTypes()) {
+                            out.append(parameterType.getSimpleName());
+                            
+                            out.append(",");
+                        }
+
+                        out.append(')');
+                        System.out.println(out);
+                    }
+                }
+            }
         }
     }
 }
