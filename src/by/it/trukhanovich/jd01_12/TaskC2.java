e875294c372dcc063a1408634f5a2fb3298a5ca7
Exedin <deathexedin@gmail.com>
2020-10-06 15:12:38 +0300

    jd01_12 TaskC2 started

--- src/by/it/trukhanovich/jd01_12/TaskC2.java
+++ src/by/it/trukhanovich/jd01_12/TaskC2.java
@@ -8,25 +8,32 @@ public class TaskC2 {
     public static void main(String[] args) {
         Set<Number> a = new HashSet<Number>(Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L));
         Set<Number> b = new HashSet<Number>(Arrays.asList(4L, 5L, 6L, 7L, 8L, 9L));
-        Set<Number> c = new HashSet<Number>(Arrays.asList(8L, 9L, 10L, 11L, 12L, 13L));
+        Set<Number> c = new HashSet<Number>(Arrays.asList(4L, 5L, 10L, 11L, 12L, 13L));
         Set<Number> d = new HashSet<Number>(Arrays.asList(2., 3., 4., 9.));
         System.out.printf("a=%s\n",a);
         System.out.printf("b=%s\n",b);
-        Set<Number> union = getUnion(a, b);
-        Set<Number> cross = getCross(a, b);
+        Set<Number> union = getUnion(a, b, c);
+        Set<Number> cross = getCross(a, b,c);
         System.out.println(union);
         System.out.println(cross);
     }
 
-    private static Set<Number> getCross(Set<Number> a, Set<Number> b) {
-        HashSet result = new HashSet(a);
-        result.retainAll(b);
+    private static Set<Number> getCross(Set<Number>...V) {
+        HashSet result = new HashSet(V[0]);
+        for (int i = 1; i < V.length; i++) {
+
+            result.retainAll(V[i]);
+        }
         return result;
     }
 
-    private static Set<Number> getUnion(Set<Number> a, Set<Number> b) {
-        HashSet result = new HashSet(a);
-        result.addAll(b);
+    private static Set<Number> getUnion(Set<Number>...V) {
+
+        HashSet result = new HashSet(V[0]);
+        for (int i = 1; i < V.length; i++) {
+
+            result.addAll(V[i]);
+        }
         return result;
 
     }
