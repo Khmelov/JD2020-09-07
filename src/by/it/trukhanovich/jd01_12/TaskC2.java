163c4493afdee0742e8a60a890f783b91cd0651b
Exedin <deathexedin@gmail.com>
2020-10-06 18:17:54 +0300

    jd01_12 TaskC3 done jd01_12 TaskB3 and TaskC2 questions

--- src/by/it/trukhanovich/jd01_12/TaskC2.java
+++ src/by/it/trukhanovich/jd01_12/TaskC2.java
@@ -6,35 +6,41 @@ import java.util.Set;
 
 public class TaskC2 {
     public static void main(String[] args) {
-        Set<Number> a = new HashSet<Number>(Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L));
-        Set<Number> b = new HashSet<Number>(Arrays.asList(4L, 5L, 6L, 7L, 8L, 9L));
-        Set<Number> c = new HashSet<Number>(Arrays.asList(4L, 5L, 10L, 11L, 12L, 13L));
-        Set<Number> d = new HashSet<Number>(Arrays.asList(2., 3., 4., 9.));
+        Set<Long> a = new HashSet<Long>(Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L));
+        Set<Long> b = new HashSet<Long>(Arrays.asList(4L, 5L, 6L, 7L, 8L, 9L));
+        Set<Long> c = new HashSet<Long>(Arrays.asList(4L, 5L, 10L, 11L, 12L, 13L));
+        Set<Double> d = new HashSet<Double>(Arrays.asList(4., 5., 20., 21.));
+
         System.out.printf("a=%s\n",a);
         System.out.printf("b=%s\n",b);
-        Set<Number> union = getUnion(a, b, c);
-        Set<Number> cross = getCross(a, b,c);
+        Set<Number> union = getUnion(a, b, c,d);
+        Set<Number> cross = getCross(a, b,c,d);
         System.out.println(union);
         System.out.println(cross);
+        Number x=10;
+        Number y=10.0;
+        if (x.doubleValue()== y.doubleValue()) System.out.println("ДА doubleValue");
+        if (x.equals(y)) System.out.println("да equals");
+        if ((Number) x==(Number) y) System.out.println("да==");
     }
 
-    private static Set<Number> getCross(Set<Number>...V) {
-        HashSet result = new HashSet(V[0]);
-        for (int i = 1; i < V.length; i++) {
-
-            result.retainAll(V[i]);
+    private static Set<Number> getCross(Set<?extends Number>...args) {
+        HashSet result = new HashSet(args[0]);
+        for (int i = 1; i < args.length; i++) {
+            result.retainAll( args[i]);
         }
         return result;
     }
 
-    private static Set<Number> getUnion(Set<Number>...V) {
+    private static Set<Number> getUnion(Set<?extends Number>...args) {
 
-        HashSet result = new HashSet(V[0]);
-        for (int i = 1; i < V.length; i++) {
+        HashSet result = new HashSet(args[0]);
+        for (int i = 1; i < args.length; i++) {
 
-            result.addAll(V[i]);
+            result.addAll(args[i]);
         }
         return result;
 
     }
+
 }
---
+++ src/by/it/trukhanovich/jd01_12/TaskC3.java
@@ -0,0 +1,51 @@
+package by.it.trukhanovich.jd01_12;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskC3 {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        //[{(3+2)/2}*10]/5+[{(3+2)/2}*10]/5
+        //{[{()}][()]{()}}
+        Scanner sc = new Scanner(System.in);
+        String s= sc.nextLine();
+        Pattern p1=Pattern.compile("[\\{\\}\\(\\)\\[\\]]");
+        Matcher m1= p1.matcher(s);
+        while (m1.find()){
+            arrayList.add(m1.group());
+        }
+        for (int i = 0; i < arrayList.size(); i++) {
+            if(arrayList.get(i).equals("(")&&arrayList.get(i+1).equals(")")){
+                arrayList.add(i+1,0);
+            }
+        }
+        boolean result= bracketsRigthOrNot(arrayList);
+
+        System.out.println(result);
+    }
+
+    private static boolean bracketsRigthOrNot(ArrayList arrayList) {
+        for(; arrayList.indexOf(0)>=0;){
+            int i1= arrayList.indexOf(0);
+            if (i1==0&&arrayList.size()==1) return true;
+            if (i1==0&&arrayList.size()!=1) return false;
+            if (i1==0&&arrayList.size()!=1)  arrayList.remove(i1);
+            if (arrayList.get(i1-1).equals("(")&& arrayList.get(i1+1).equals(")")) {
+                arrayList.remove(i1+1);
+                arrayList.remove(i1-1); continue;}
+            else if (arrayList.get(i1-1).equals("[")&& arrayList.get(i1+1).equals("]")) {
+                arrayList.remove(i1+1);
+                arrayList.remove(i1-1);
+                continue;}
+            else if (arrayList.get(i1-1).equals("{")&& arrayList.get(i1+1).equals("}")) {
+                arrayList.remove(i1+1);
+                arrayList.remove(i1-1);
+                continue;}
+            else if(arrayList.size()==1&& arrayList.get(0).equals(0)) return true;
+            else arrayList.remove(i1);
+        }
+        return false;
+    }
+}
