d9e2925d259ed811fb9d20b19d085e299c0fdb20
Exedin <deathexedin@gmail.com>
2020-10-06 15:03:09 +0300

    jd01_12 TaskC2 started

--- src/by/it/trukhanovich/jd01_12/TaskC2.java
+++ src/by/it/trukhanovich/jd01_12/TaskC2.java
@@ -7,8 +7,8 @@ import java.util.Set;
 public class TaskC2 {
     public static void main(String[] args) {
         Set<Number> a = new HashSet<Number>(Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L));
-        Set<Number> b = new HashSet<Number>(Arrays.asList(4, 3, 5, 6, 7, 8));
-        Set<Number> c = new HashSet<Number>(Arrays.asList(0., 1., 2., 3., 4., 5.));
+        Set<Number> b = new HashSet<Number>(Arrays.asList(4L, 5L, 6L, 7L, 8L, 9L));
+        Set<Number> c = new HashSet<Number>(Arrays.asList(8L, 9L, 10L, 11L, 12L, 13L));
         Set<Number> d = new HashSet<Number>(Arrays.asList(2., 3., 4., 9.));
         System.out.printf("a=%s\n",a);
         System.out.printf("b=%s\n",b);
