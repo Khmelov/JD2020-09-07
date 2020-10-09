43332ed3ae1f1991371da8c64acde4c4d3a1cf0b
Exedin <deathexedin@gmail.com>
2020-10-10 00:53:02 +0300

    jd01_14 TaskB started

--- src/by/it/trukhanovich/jd01_13/TaskC.java
+++ src/by/it/trukhanovich/jd01_13/TaskC.java
@@ -7,7 +7,7 @@ public class TaskC {
     private static ArrayList arrayList=new ArrayList();
     private static Scanner sc;
     static int i=0;
-    public static void main(String[] args) throws NumberFormatException, InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
             TaskC.sc = new Scanner(System.in);
             readData();
     }
@@ -26,8 +26,9 @@ public class TaskC {
                     }
                     i++;
                     readData();
-                } else throw new NumberFormatException();
+                } else throw new RuntimeException(e);
             }
+
         }
     }
 }
