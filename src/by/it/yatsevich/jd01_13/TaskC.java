7dc5d42b3ab42d3c0f60959652e6174032af1e68
lu55y <mu11er100500@gmail.com>
2020-10-10 15:15:25 +0300

    jd01_13 C

--- src/by/it/yatsevich/jd01_13/TaskC.java
+++ src/by/it/yatsevich/jd01_13/TaskC.java
@@ -1,37 +1,36 @@
 package by.it.yatsevich.jd01_13;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 class TaskC {
     static int value = 0;
     static List<Double> temp = new LinkedList<>();
+    static String s;
 
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        readData(scan);
-    }
-
-    private static void readData(Scanner scan) throws InterruptedException {
         while (value < 5) {
             try {
-                System.out.println("value = "+value);
-                String i = scan.next();
-                System.out.println(i);
-                Double j = Double.parseDouble(i);
-                temp.add(j);
-
+                s = scan.next();
+                readData();
             } catch (NumberFormatException e) {
                 Thread.sleep(100);
-                Collections.reverse(temp);
-                Iterator it=temp.iterator();
-                while (it.hasNext()){
-                    Object next = it.next();
-                    System.out.print(" "+next);
-                Collections.reverse(temp);
-                }
+                for (int i = temp.size() - 1; i >= 0; i--) {
+                    System.out.print(" " + temp.get(i));
+                }System.out.println();
                 value++;
             }
         }
     }
+
+
+    static void readData() {
+        System.out.println(s);
+        Double j = Double.parseDouble(s);
+        temp.add(j);
+    }
 }
+
+
