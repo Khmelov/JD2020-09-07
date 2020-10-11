18b1340d5c806d640a8cc0a1e6ee9cf28da5a2c2
Artkulik <artkulik@inbox.ru>
2020-10-11 15:27:07 +0300

    JD01_14

--- src/by/it/kulik/jd01_14/TaskB.java
+++ src/by/it/kulik/jd01_14/TaskB.java
@@ -1,28 +1,39 @@
 package by.it.kulik.jd01_14;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class TaskB {
     public static void main(String[] args) {
         String src = System.getProperty("user.dir") + "/src/by/it/kulik/";
         String filename = src + "JD01_14/text.txt";
         File f = new File(filename);
-        FileReader is = null;
-        int b, count = 0;
+        BufferedReader BR = null;
+        int b,n = 0, count = 0;
         try {
-            is = new FileReader(f);
-            while ((b = is.read()) != -1) {
+            BR = new BufferedReader(new FileReader(f));
+            while ((b = BR.read()) != -1) {
                 System.out.print((char) b);
+                if (b == ','){
+                n++;
+                }
                 count++;
+                }
+
+            String res=BR.readLine();
+            while (res!=null){
+                res= BR.readLine();
+                System.out.println(Arrays.toString(res.split(" ")));
             }
+            System.out.println();
+            System.out.println("\npunctuation marks="+n);
             System.out.println("\n Число байт=" + count);
-            System.out.println(" Кодировка " + is.getEncoding());
-        } catch (IOException e) {
+            } catch (IOException e) {
             System.err.println(" Ошибка файла:" + e);
         } finally {
             try {
-                if (is != null) {
-                    is.close();
+                if ( BR!= null) {
+                    BR.close();
                 }
             } catch (IOException e) {
                 System.err.println("ошибка закрытия:" + e);
