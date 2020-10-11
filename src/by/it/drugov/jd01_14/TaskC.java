88e2d00a514c2f82d968cd4f0a80eeb62b56e22f
antikd8 <antikd8888@gmail.com>
2020-10-12 01:45:26 +0300

    jd01_14 TaskA TaskB TaskC

--- src/by/it/drugov/jd01_14/TaskC.java
+++ src/by/it/drugov/jd01_14/TaskC.java
@@ -2,19 +2,35 @@ package by.it.drugov.jd01_14;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class TaskC {
     public static void main(String[] args) {
-        printDir();
-        printFiles();
-        File fw = new File("resultTaskC.txt");
+        StringBuilder line = new StringBuilder();
+        line.append(printDir()).append(printFiles());
+        File fw = new File(dir(TaskC.class) + "resultTaskC.txt");
         FileWriter os = null;
+        try {
+            fw.createNewFile();
+            os = new FileWriter(fw);
+            os.write(line.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
 
     }
 
-    private static void printFiles() {
+    private static String printFiles() {
         File file = new File(dirPackages(TaskC.class));
         File[] files = file.listFiles();
         StringBuilder line = new StringBuilder();
@@ -22,26 +38,33 @@ public class TaskC {
             line.append(Arrays.toString(files[i].list()));
         }
         String newLine = line.toString();
+        line.replace(0,line.length(),"");
         String[] lines = newLine
-                .replace("[", ", ")
+                .replace("][", ", ")
                 .replace("]", "")
-                .trim()
+                .replace("[", "")
                 .split(", ");
         for (int i = 0; i < lines.length; i++) {
-            System.out.println("file:" + lines[i]);
+            lines[i]= "file:" + lines[i]+"\n";
+            line.append(lines[i]);
+            System.out.println(line);
         }
+        return line.toString();
     }
 
-    //  private static String dirFiles(Class<?> cl) {
-    //  }
-
-    private static void printDir() {
+    private static String printDir() {
         File file = new File(dirPackages(TaskC.class));
         File[] files = file.listFiles();
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < files.length; i++) {
             String line = files[i].toString();
-            System.out.println("dir:" + line.replace(files[i].getParent(), "").replace(File.separator, ""));
+            line = line.replace(files[i].getParent(), "").
+                    replace(File.separator, "");
+            line = "dir:" + line + "\n";
+            System.out.print(line);
+            result.append(line);
         }
+        return result.toString();
     }
 
     private static String dirPackages(Class<?> cl) {
@@ -51,5 +74,11 @@ public class TaskC {
                 .replace(".", File.separator);
         File file = new File(path + classPath);
         return file.getParent();
+    }
+
+    private static String dir(Class<?> cl) {
+        String path = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String classPath = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
+        return path + classPath;
     }
 }
