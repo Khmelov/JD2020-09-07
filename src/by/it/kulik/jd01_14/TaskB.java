9714c61b3c9a608c5ad054b092d3ecdba6f86959
Artkulik <artkulik@inbox.ru>
2020-10-11 17:26:46 +0300

    JD01_14B

--- src/by/it/kulik/jd01_14/TaskB.java
+++ src/by/it/kulik/jd01_14/TaskB.java
@@ -1,33 +1,34 @@
 package by.it.kulik.jd01_14;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class TaskB {
     public static void main(String[] args) {
         String src = System.getProperty("user.dir") + "/src/by/it/kulik/";
         String filename = src + "JD01_14/text.txt";
         File f = new File(filename);
-        BufferedReader BR = null;
-        int b,n = 0, count = 0;
+        BufferedReader BR=null;
+        int n = 0, marks = 0;
         try {
             BR = new BufferedReader(new FileReader(f));
-            while ((b = BR.read()) != -1) {
-                System.out.print((char) b);
-                if (b == ','){
-                n++;
+            String res;
+            while ((res=BR.readLine())!=null){
+                Pattern p=Pattern.compile("[!.,-]");
+                Matcher m=p.matcher(res);
+                while(m.find()){
+                    marks++;
                 }
-                count++;
+                Pattern p1=Pattern.compile("[ .]");
+                Matcher m1=p1.matcher(res);
+                while(m1.find()){
+                    n++;
                 }
-
-            String res=BR.readLine();
-            while (res!=null){
-                res= BR.readLine();
-                System.out.println(Arrays.toString(res.split(" ")));
             }
             System.out.println();
-            System.out.println("\npunctuation marks="+n);
-            System.out.println("\n Число байт=" + count);
+            System.out.println("\npunctuation marks="+marks+" words="+n);
             } catch (IOException e) {
             System.err.println(" Ошибка файла:" + e);
         } finally {
