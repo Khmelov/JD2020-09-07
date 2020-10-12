4e026561107041e6cc7cc29161075047a87350f0
lu55y <mu11er100500@gmail.com>
2020-10-12 03:24:35 +0300

    jd01_14

--- src/by/it/yatsevich/jd01_12/TaskC1.java
+++ src/by/it/yatsevich/jd01_12/TaskC1.java
@@ -6,12 +6,32 @@ public class TaskC1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ArrayList<String> temp = new ArrayList<>();
-        Map<Long, String> longStringMap;
+        HashMap<Long, String> textMap = new HashMap<>();
         String str;
-        scan.useDelimiter("[/\\s\\.—,:;?(){}]");
-        while (!(str=scan.nextLine()).equals("end")){
+        scan.useDelimiter("[/\\s—,:;?(){}]");
+        while (!(str = scan.nextLine()).equals("end")) {
             temp.add(str);
         }
 
+        for (String s : temp) {
+            long value = 123 + (int) (Math.random() * 1000000);
+            if (textMap.containsKey(value)) {
+                value += 1;
+                textMap.put(value, s);
+            }
+            textMap.put(value, s);
+        }
+        System.out.println(textMap);
+        textMap.entrySet().stream().sorted(Map.Entry.comparingByValue());
+//        System.out.println(textMap);
+        Iterator<Map.Entry<Long, String>> iterator = textMap.entrySet().iterator();
+        HashSet<String> objects = new HashSet<>();
+        while (iterator.hasNext()) {
+            Map.Entry<Long, String> next = iterator.next();
+            if (!objects.add(next.getValue())) iterator.remove();
+        }
+        System.out.println(textMap);
     }
 }
+
+
