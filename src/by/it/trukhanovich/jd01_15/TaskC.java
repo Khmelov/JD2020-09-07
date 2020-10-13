371715d9f2c231e3d16ac3b593632f5686d79f26
Exedin <deathexedin@gmail.com>
2020-10-13 11:37:58 +0300

    jd01_15 TaskB in progress jd01_15 TaskC done

--- src/by/it/trukhanovich/jd01_15/TaskC.java
+++ src/by/it/trukhanovich/jd01_15/TaskC.java
@@ -1,7 +1,9 @@
 package by.it.trukhanovich.jd01_15;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,44 +12,60 @@ public class TaskC {
         String path = getPath(TaskC.class);
         File file = new File(path);
         System.out.println(file.getPath());
+        Scanner sc = new Scanner(System.in);
         for(;;){
-            Scanner sc = new Scanner(System.in);
-            String exception =sc.nextLine();
-            List<String> strings = Arrays.asList(exception.split(" "));
+            String exp =sc.nextLine();
+            List<String> strings = Arrays.asList(exp.split(" "));
             File[] files = file.listFiles();
             if (strings.get(0).equals("end")){break;}
             else if (strings.get(0).equals("dir")){
-                System.out.println("содержимое "+file.getName());
-                for (File f1 : files) {
-                    if (f1.isDirectory()){
-                        System.out.println("DIR: "+f1.getName());;
-                    }
-                    else System.out.println("FILE: "+f1.getName());
-                }
+                getDir(path, files);
             }
             else if (strings.get(0).equals("cd")){
-                    if(strings.get(1).equals("..")){
-                        path=file.getParent();
-                        file= new File(path);
-//                        System.out.println(path);
-                    }
-                    else {
-                        String path1 = path + File.separator + strings.get(1);
-                        file= new File(path1);
-                        if (file.exists()){
-                            path=path1;
-//                            System.out.println(path);
-                        }
-                        if (!file.exists()){
-                            file= new File(path);
-//                            System.out.println(path);
-                        }
-                    }
+                path = getAddress(path, file, strings);
+                file= new File(path);
             }
             else System.out.println(path);
         }
 
     }
+
+    private static String getAddress(String path, File file, List<String> strings) {
+        if(strings.get(1).equals("..")){
+            path = file.getParent();
+            System.out.println(path);
+        }
+        else {
+            String path1 = path + File.separator + strings.get(1);
+            if (file.exists()){
+                path =path1;
+                System.out.println(path);
+            }
+            if (!file.exists()){
+                System.out.println(path);
+            }
+        }
+        return path;
+    }
+
+    private static void getDir(String path, File[] files) {
+        System.out.println("содержимое "+ path);
+        for (File f1 : files) {
+            if (f1.isDirectory()){
+                Date date= new Date(f1.lastModified());
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+                System.out.print(simpleDateFormat.format(date)+"\t\t");
+                System.out.printf("<DIR>           %s\n",f1.getName());
+            }
+            else {
+                Date date= new Date(f1.lastModified());
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+                System.out.print(simpleDateFormat.format(date)+"\t\t");
+                System.out.printf("%15d %s\n",f1.length(), f1.getName());
+            }
+        }
+    }
+
     private static String getPath(Class<?> taskClass) {
         String rootProject = System.getProperty("user.dir");
         String path = taskClass.getName().
