3fb9e6684edd7d672060e1a83f62ea69d82765eb
lu55y <mu11er100500@gmail.com>
2020-10-14 18:09:10 +0300

    jd01_15

--- src/by/it/yatsevich/jd01_15/TaskB.java
+++ src/by/it/yatsevich/jd01_15/TaskB.java
@@ -1,5 +1,13 @@
 package by.it.yatsevich.jd01_15;
 
 class TaskB {
+    public static void main(String[] args) {
+        //Print to console "Hello, "
+        System.out.printf("Hello, ");
+        //Print to console "Hello, world"
+        String s= "world";
+        System.out.println("Hello, " +s);
+    }
+
 
 }
