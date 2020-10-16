70817879a685359b6447d8ef5245f535ce3dfef6
lu55y <mu11er100500@gmail.com>
2020-10-16 16:25:27 +0300

    jd02_1 a_b

---
+++ src/by/it/yatsevich/jd02_01/Buyer.java
@@ -0,0 +1,59 @@
+package by.it.yatsevich.jd02_01;
+
+class Buyer extends Thread implements IBayer,IUseBasket {
+
+    public Buyer(int number) {
+        super("Bayer №"+number);
+    }
+
+    @Override
+    public void run() {
+        enterToMarket();
+        takeBasket();
+        Supervisor.COUNTS_OF_GOODS=Helper.getRandomGoods();
+        chooseGoods();
+        putGoodsToBasket();
+        goOut();
+//        Supervisor.COUNTS_OF_GOODS=1;
+        Supervisor.BUYER_IN_THE_SHOP--;
+    }
+
+    @Override
+    public void enterToMarket() {
+        System.out.printf("%s enter to market\n",this);
+    }
+
+    @Override
+    public void chooseGoods() {
+        System.out.printf("%s starting choose\n",this);
+//        Supervisor.COUNTS_OF_GOODS=Helper.getRandomGoods();
+        int timeout=Supervisor.COUNTS_OF_GOODS *Helper.getRandom(500,2000);
+        Helper.buyerSleep(timeout);
+        System.out.printf("%s wonna %d good\n",this,Supervisor.COUNTS_OF_GOODS);
+
+    }
+
+    @Override
+    public void goOut() {
+        System.out.printf("%s buyer go out\n",this);
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+
+    @Override
+    public void takeBasket() {
+        System.out.printf("%s buyer get basket\n",this);
+    }
+
+    @Override
+    public void putGoodsToBasket() {
+        System.out.printf("%s put %d goods to basket\n",this,Supervisor.COUNTS_OF_GOODS);
+        Basket.putToBasket(Supervisor.COUNTS_OF_GOODS);
+        System.out.println(Basket.temp);
+        System.out.printf("Total purchase value : %3d$\n",Basket.costOfGoods);
+        System.out.printf("%s finished choose\n",this);
+    }
+}
---
+++ src/by/it/yatsevich/jd02_01/IBayer.java
@@ -0,0 +1,8 @@
+package by.it.yatsevich.jd02_01;
+
+interface IBayer {
+
+    void enterToMarket();
+    void chooseGoods();
+    void goOut();
+}
