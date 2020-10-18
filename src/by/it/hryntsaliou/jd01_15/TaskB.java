741d91a6efe130ec51b001d2133267ffb9df6dd4
Nikita <hrynts2002@gmail.com>
2020-10-18 21:59:55 +0300

    jd02_02

--- src/by/it/hryntsaliou/jd01_15/TaskB.java
+++ src/by/it/hryntsaliou/jd01_15/TaskB.java
@@ -12,69 +12,91 @@ class TaskB {
         String filename = getPath(TaskB.class) + "TaskB.java";
         File file = new File(filename);
         StringBuilder codeText;
-        try (FileReader fileReader = new FileReader(filename)) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
             char[] chars = new char[(int) file.length()];
-            fileReader.read(chars);
+            bufferedReader.read(chars);
             codeText = new StringBuilder(String.valueOf(chars));
         } catch (IOException e) {
             throw new RuntimeException();
         }
-        String path = getPath(TaskB.class) + "TaskB.txt";
         deleteSingleComment(codeText);
         deleteMultiComment(codeText);
-        deleteJavaDoc(codeText);
-//        System.out.println(codeText);
-//        try (PrintWriter printWriter = new PrintWriter(path)) {
-//            printWriter.print(codeText);
-//        } catch (FileNotFoundException e) {
-//            throw new RuntimeException();
-//        }
-//        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-//            while (reader.ready()) {
-//                System.out.println(reader.readLine());
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException();
-//        }
+        String path = getPath(TaskB.class) + "TaskB.txt";
+        try (PrintWriter printWriter = new PrintWriter(path)) {
+            printWriter.print(codeText);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            while (reader.ready()) {
+                System.out.println(reader.readLine());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 
-    private static void deleteJavaDoc(StringBuilder codeText) {
-    }
+    //London is the capital of Great Britain
+    //I want to break free
+    //I want to break free
+    //I wand to break free from your lies
+    //You're so self satisfied i don't need you
 
     private static void deleteMultiComment(StringBuilder codeText) {
+        int length = codeText.length();
+        for (int i = 0; i < codeText.length(); i++) {
+            if (codeText.charAt(i) == '/' && codeText.charAt(i + 1) == '*') {
+                int begin = i;
+                for (int j = begin; j < codeText.length(); j++) {
+                    if (codeText.charAt(j) == '*' && codeText.charAt(j + 1) == '/') {
+                        int end = j + 1;
+                        for (int k = begin; k <= end; k++) {
+                            codeText.replace(k, k + 1, " ");
+                            i--;
+                        }
+                        codeText.delete(begin, end+1);
+                        i--;
+                        codeText.setLength(length);
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     private static void deleteSingleComment(StringBuilder codeText) {
-        String[] lines = codeText.toString().split("\n");
-        StringBuilder newText = new StringBuilder();
-        for (int i = 0; i < lines.length; i++) {
-            if(!lines[i].contains("//")) {
-                newText.append(lines[i]);
+//        char[] chars = new char[codeText.length()];
+//        codeText.getChars(0, codeText.length(), chars, 0);
+        int length = codeText.length();
+        for (int i = 0; i < codeText.length(); i++) {
+            if (codeText.charAt(i) == '/' && codeText.charAt(i + 1) == '/') {
+                int begin = i;
+                for (int j = begin; j < codeText.length(); j++) {
+                    if (codeText.charAt(j) == '\n') {
+                        int end = j;
+                        for (int k = begin; k <= end; k++) {
+                            if (k == end) break;
+                            codeText.replace(k, k + 1, " ");
+                        }
+                        codeText.delete(begin, end);
+                        i--;
+                        codeText.setLength(length);
+                        break;
+                    }
+                }
             }
         }
-        codeText = newText;
-        System.out.println(codeText);
     }
-
-    //codeText = new StringBuilder(String.valueOf(chars))
-        /*for (int i = 0; i < chars.length; i++) {
-            System.out.println(chars[i]);
-        }*/
-
-    //It's my code
-    //I love Java
-
-
     /*Html это язык программирования,
     который объединяет людей по всему
     миру!!!
      */
 
-
     /*You've got something i need
     In this world full of people
     There's one killing me
      */
+
     private static String getPath(Class<TaskB> taskBClass) {
         String rootProject = System.getProperty("user.dir");
         String relativePath = taskBClass.getName()
