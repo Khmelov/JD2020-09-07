7e365807859fbd3728fc8922e8d6760eced4d04a
lu55y <mu11er100500@gmail.com>
2020-10-19 00:15:01 +0300

    jd02_01

--- src/by/it/yatsevich/jd02_01/Helper.java
+++ src/by/it/yatsevich/jd02_01/Helper.java
@@ -4,7 +4,10 @@ import java.util.Random;
 
 class Helper {
     private static final Random random = new Random();
-
+    static int start = 500;
+    static int stop = 2000;
+    static double pensionValue =1.5;
+    static boolean pensioner =false;
     static int getRandom(int start,int stop){
         return start + random.nextInt(stop-start+1);
     }
@@ -12,7 +15,6 @@ class Helper {
     static int getRandom(int max){
         return getRandom(0,max);
     }
-    static int getRandomGoods(){return 1 + random.nextInt(4);}
 
     static void buyerSleep(int millis){
         try {
