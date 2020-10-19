7d578bdcd0d17f8f402c65c651a91d5134b35b5e
Exedin <deathexedin@gmail.com>
2020-10-20 00:27:14 +0300

    jd02_03 TaskA done

---
+++ src/by/it/trukhanovich/jd02_03/QueueCashier.java
@@ -0,0 +1,32 @@
+package by.it.trukhanovich.jd02_03;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class QueueCashier {
+
+    private QueueCashier() {
+    }
+    private final static BlockingDeque<Cashier> QUEUE_CASHIER=new LinkedBlockingDeque<>(5);
+
+    static void add (Cashier cashier) {
+        try {
+            QUEUE_CASHIER.putLast(cashier);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static Cashier extract () {
+            return QUEUE_CASHIER.pollFirst();
+    }
+
+//    public static int getSizeQUEUE_CASHIER() {
+//        return QUEUE_CASHIER.size();
+//    }
+
+//    public static Deque<Cashier> getQueueCashier() {
+//        return QUEUE_CASHIER;
+//    }
+}
