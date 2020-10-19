657f29813f3d9b99d7edcf62788ab4a2bf9e5630
IT-Academy <git@itacademy.by>
2020-10-19 21:37:02 +0300

    classwork 02_03

---
+++ src/by/it/_akhmelev_/jd02_03/QueueBuyers.java
@@ -0,0 +1,28 @@
+package by.it._akhmelev_.jd02_03;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+class QueueBuyers {
+
+    private QueueBuyers() {
+    }
+
+    private final static BlockingDeque<Buyer> QUEUE = new LinkedBlockingDeque<>(30);
+
+    static void add(Buyer buyer) {
+        try {
+            QUEUE.putLast(buyer);
+        } catch (InterruptedException e) {
+           throw new RuntimeException(e);
+        }
+    }
+
+    static Buyer extract() {
+            return QUEUE.pollFirst();
+    }
+
+
+}
