7a8b33bc527536f7d41c482817fd9bf3d4542c16
TarasevichP <taraspasha@gmail.com>
2020-10-25 12:56:42 +0300

    Task 14(taskB),15(taskB), correct exceptions for my code

--- src/by/it/tarasevich/jd01_15/TaskB.java
+++ src/by/it/tarasevich/jd01_15/TaskB.java
@@ -1,6 +1,7 @@
 package by.it.tarasevich.jd01_15;
 
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -13,97 +14,86 @@ import java.io.*;
 
 
 public class TaskB {
-    /* comments
-     *
-     **/
+
     public static void main(String[] args) {
-        String path = getPath().concat(TaskB.class.getSimpleName()).concat(".java");
 
-        StringBuilder stringBuilder = new StringBuilder();
 
+//Comment second one string too
+
+
+        /*
+        *
+        * four strings for comments
+        * */
+
+        StringBuilder strB = new StringBuilder();
+        String delimiter = "";
+        String path = getPath() + "TaskB.java";
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
             while (bufferedReader.ready()) {
-                stringBuilder.append(bufferedReader.readLine());
-                stringBuilder.append("\n");
+                strB.append(bufferedReader.readLine()).append(delimiter).append("\n");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String s = stringBuilder.toString();
+//Comment first one string
 
-        int b;
-        StringBuilder test = new StringBuilder();
-        try (final BufferedReader bufferedReader = new BufferedReader(new StringReader(s))) {
-            boolean a1 = false;
- //однострочный комментарий
-            boolean comment = false;
-            while ((b = bufferedReader.read()) != -1) {
-                if (!comment) {
-                    test.append((char) b);
-                }
-                if (!comment && a1 && b != '*') {
-                    a1 = false;
-                    continue;
-                }
-                if (!comment && b == '/') {
-                    a1 = true;
-                    continue;
-                }
-                if (!comment && (b == '*') && (a1 = true)) {
-                    comment = true;
-                    a1 = false;
-                    test.delete(test.length() - 3, test.length()).append("\n");
-                }
-// одностройчный комментарий номер два
-            }
+        clearSingleComment(strB);
+        clearMultiCommentsForCode(strB);
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter(getPath() + "TaskB.txt"))) {
+            bufferedWriter.append(strB);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String s1 = test.toString();
-        StringBuilder result = new StringBuilder();
-        try (final BufferedReader bufferedReader = new BufferedReader(new StringReader(s1))) {
-            boolean b1 = false;
-            while ((b = bufferedReader.read()) != 1) {
-                result.append((char) b);
-                if (b1 && b != '/') {
-                    b1 = false;
-                    continue;
-                }
-                if (b == '/') {
-                    if (b1 = true) {
-                        continue;
-                        /* comments
-                         *
-                         **/
 
-                    }
-                    if (b1 = true) {
-                        result.delete(test.length() - 2, test.length()).append("\n");
+        /*Some text
+         * five strings for comments
+         *
+         *
+         * */
+    }
+
+    private static void clearSingleComment(StringBuilder lines) {
+
+        for (int i = 0;i < lines.length();i++) {
+            if (lines.charAt(i) == '/' && lines.charAt(i + 1) == '/') {
+                for (int j = i; j < lines.length(); j++) {
+                    if (lines.charAt(j) == '\n') {
+                        lines.delete(i, j);
+                        System.out.println(lines);
+                        break;
                     }
                 }
             }
-            System.out.println(result);
-            printTXT(result, getPath().concat(TaskB.class.getSimpleName()).concat(".txt"));
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
-     private static String getPath() {
-        String rootProgect = System.getProperty("user.dir");
-        String path = TaskB.class.getName().replace(TaskB.class.getSimpleName(), "").
-                replace(".", File.separator);
 
-        return rootProgect + File.separator + "srs" + File.separator + path;
-    }
+    private static void clearMultiCommentsForCode(StringBuilder lines) {
+        for (int i = 0;i < lines.length();i++) {
+            if (lines.charAt(i) == '/' && lines.charAt(i + 1) == '*') {
+                for (int j = i; j < lines.length(); j++) {
+                    if (lines.charAt(j) == '*' && lines.charAt(j + 1) == '/') {
+                        lines.delete(i, j + 2);
+                        System.out.println(lines);
+                        break;
+                    }
+                }
+            }
 
-     static void printTXT(StringBuilder stringBuilder, String path) {
-        try (PrintWriter printWriter = new PrintWriter(path)) {
-            printWriter.println(stringBuilder);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         }
 
     }
+
+    private static String getPath() {
+        String projectDir = System.getProperty("user.dir");
+        String relativePath = TaskB.class.getName()
+                .replace(TaskB.class.getSimpleName(), "")
+                .replace(".", File.separator);
+        return projectDir + File.separator + "src" + File.separator + relativePath;
+    }
+
 }
+
