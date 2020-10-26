3862afea039b49934f0e27d99b588d149699561f
Nikita <hrynts2002@gmail.com>
2020-10-26 03:03:02 +0300

    jd02_04

---
+++ src/by/it/hryntsaliou/calc/ConsoleRunner.java
@@ -0,0 +1,19 @@
+package by.it.hryntsaliou.calc;
+
+import java.util.Scanner;
+
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Parser parser = new Parser();
+        Printer printer = new Printer();
+        for(;;) {
+            String expression = scanner.nextLine();
+            if (expression.equals("end")) {
+                break;
+            }
+            Var result = parser.calc(expression);
+            printer.print(result);
+        }
+    }
+}
