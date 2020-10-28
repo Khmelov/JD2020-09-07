95515116e29a92db9d36ffd9ceb16453639d1bb1
nosorog7798 <perec2522559vlad@gmail.com>
2020-10-28 15:58:57 +0300

    02.01-02.03

---
+++ src/by/it/ilysuchanka/jd02_01/IUseBasket.java
@@ -0,0 +1,6 @@
+package by.it.ilysuchanka.jd02_01;
+
+public interface IUseBasket {
+    void takeBasket(); //покупатель взял корзину
+    void putGoodsToTheBasket(); //покупатель положил выбранный товар в корзину
+}
---
+++ src/by/it/ilysuchanka/jd02_03/IUseBasket.java
@@ -0,0 +1,8 @@
+package by.it.ilysuchanka.jd02_03;
+
+interface IUseBasket extends IBuyer {
+
+    void takeBasket();
+    void putGoodsToBasket();
+
+}
