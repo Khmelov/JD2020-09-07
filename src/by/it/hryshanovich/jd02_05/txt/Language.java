707380ba0acc660f8d7325ca1cd4dd26128e8762
Hryshanovich <alexandars6236@gmail.com>
2020-10-29 12:40:24 +0300

    jd02_4 A jd02_5 A

---
+++ src/by/it/hryshanovich/jd02_05/txt/Language.java
@@ -0,0 +1,24 @@
+package by.it.hryshanovich.jd02_05.txt;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public enum Language {
+    RB;
+    private static final String KEYWORDS = "by.it.drugov.jd02_05.resources.keywords";
+    private Locale locale;
+    private ResourceBundle resourceBundle;
+
+    Language() {
+        setLocale(new Locale("en","UK"));
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+        resourceBundle = ResourceBundle.getBundle(KEYWORDS, locale);
+    }
+
+    public String get(String key) {
+        return resourceBundle.getString(key);
+    }
+}
