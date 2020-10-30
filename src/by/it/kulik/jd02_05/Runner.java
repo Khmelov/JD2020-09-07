2a58c9fd16522353fd3adebf6c758077661d817a
Artkulik <artkulik@inbox.ru>
2020-10-30 14:53:31 +0300

    JD02_05a

---
+++ src/by/it/kulik/jd02_05/Runner.java
@@ -0,0 +1,20 @@
+package by.it.kulik.jd02_05;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Runner {
+    public static void main(String[] args) {
+        ResMan res = ResMan.INSTANCE;
+        if(args.length==2){
+           Locale locale= new Locale(args[0], args[1]);
+           res.setLocale(locale);
+        }
+        System.out.println(res.get(Message.hello));
+        System.out.println(res.get(Message.question));
+        System.out.println(res.get(User.firstName));
+        System.out.println(res.get(User.lastName));
+    }
+}
