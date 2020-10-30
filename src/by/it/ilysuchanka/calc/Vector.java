8941471e93bf8ff705a5183049b86c60f5e8fbe6
nosorog7798 <29071996Dkfl>
2020-10-30 17:00:36 +0300

    02.05,02.06,calc

---
+++ src/by/it/ilysuchanka/calc/Vector.java
@@ -0,0 +1,40 @@
+package by.it.ilysuchanka.calc;
+
+class Vector extends Var {
+    private double[] value;
+    Vector(double[] value) {
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        String delimiter = "";
+        for (double element : value) {
+            sb.append(delimiter).append(element);
+            delimiter = ", ";
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+    Vector(Vector vector) {
+        this.value = vector.value;
+    }
+
+    Vector(String str) {
+        String[] strings = str
+                .replace('{', ' ')
+                .replace('}', ' ')
+                .trim()
+                .split(",\\s*");
+        double[] array = new double[strings.length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Double.parseDouble(strings[i]);
+        }
+        this.value = array;
+    }
+
+        }
+
+
+
+
---
+++ src/by/it/ilysuchanka/jd02_05/User.java
@@ -0,0 +1,6 @@
+package by.it.ilysuchanka.jd02_05;
+
+public interface User {
+    String firstName="user.firstName";
+    String lastName="user.lastName";
+}
