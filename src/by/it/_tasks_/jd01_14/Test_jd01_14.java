2a73ca3f9ac8e823d7f73beb66a3e21a6e618a93
Alexander Khmelov <akhmelev@gmail.com>
2020-11-19 00:49:57 +0300

    after create parent (by.it.HomeWork) but don't check it

--- src/by/it/_tasks_/jd01_14/Test_jd01_14.java
+++ src/by/it/_tasks_/jd01_14/Test_jd01_14.java
@@ -1,6 +1,7 @@
 package by.it._tasks_.jd01_14;
 
 
+import by.it.HomeWork;
 import org.junit.Test;
 
 import java.io.*;
@@ -18,16 +19,16 @@ import static org.junit.Assert.assertFalse;
 @SuppressWarnings("all")
 
 //поставьте курсор на следующую строку и нажмите Ctrl+Shift+F10
-public class Test_jd01_14 {
+public class Test_jd01_14 extends HomeWork {
 
     @Test(timeout = 1500)
     public void testTaskA() throws Exception {
-        Test_jd01_14 run = run("");
+        HomeWork run = run("");
         StringBuilder sb = new StringBuilder();
         //читаем файл с числами
         try (DataInputStream inp = new DataInputStream
                 (new BufferedInputStream
-                        (new FileInputStream(dir(Test_jd01_14.class) + "dataTaskA.bin"))
+                        (new FileInputStream(dir(this.getClass()) + "dataTaskA.bin"))
                 );
         ) {
             double sum = 0;
@@ -41,7 +42,7 @@ public class Test_jd01_14 {
             run.include(sb.toString().trim()); //проверка строки из 20 чисел
             run.include("avg=" + sum / count); //проверка вывода среднего арифметического
 
-            Scanner scanner = new Scanner(new File(dir(Test_jd01_14.class) + "resultTaskA.txt"));
+            Scanner scanner = new Scanner(new File(dir(this.getClass()) + "resultTaskA.txt"));
             //проверка соответсвия вывода и содержимого файла отчета resultTaskA.txt
             while (scanner.hasNext()) {
                 run.include(scanner.nextLine());
@@ -52,12 +53,12 @@ public class Test_jd01_14 {
 
     @Test(timeout = 1500)
     public void testTaskB() throws Exception {
-        Test_jd01_14 run = run("");
+        HomeWork run = run("");
         run.include("words=157"); //слов должно быть 157
         run.include("marks=32");  //знаков должно быть 32 (исправлено 21.01.2018)
         StringBuilder sb = new StringBuilder();
         //читаем файл с числами
-        Scanner scanner = new Scanner(new File(dir(Test_jd01_14.class) + "resultTaskB.txt"));
+        Scanner scanner = new Scanner(new File(dir(this.getClass()) + "resultTaskB.txt"));
         //проверка соответствия вывода и содержимого файла отчета resultTaskB.txt
         while (scanner.hasNext()) {
             run.include(scanner.nextLine());
@@ -67,9 +68,9 @@ public class Test_jd01_14 {
 
     @Test(timeout = 1500)
     public void testTaskC() throws Exception {
-        Test_jd01_14 run = run("");
-        showDir(dir(Test_jd01_14.class)+"..",run);
-        Scanner scanner = new Scanner(new File(dir(Test_jd01_14.class) + "resultTaskC.txt"));
+        HomeWork run = run("");
+        showDir(dir(this.getClass())+"..",run);
+        Scanner scanner = new Scanner(new File(dir(this.getClass()) + "resultTaskC.txt"));
         //проверка соответствия вывода и содержимого файла отчета resultTaskC.txt
         scanner.nextLine(); //пропуск потенциально возможного dir:..
         while (scanner.hasNext()) {
@@ -78,7 +79,7 @@ public class Test_jd01_14 {
         scanner.close();
     }
 
-    private static void showDir(String path, Test_jd01_14 run) {
+    private static void showDir(String path, HomeWork run) {
         File p = new File(path);
         String name = p.getName();
         if (p.isFile()) {
@@ -99,191 +100,5 @@ public class Test_jd01_14 {
                 cl.getName().replace(cl.getSimpleName(), "").replace('.', '/');
     }
 
-/*
-===========================================================================================================
-НИЖЕ ВСПОМОГАТЕЛЬНЫЙ КОД ТЕСТОВ. НЕ МЕНЯЙТЕ В ЭТОМ ФАЙЛЕ НИЧЕГО.
-Но изучить как он работает - можно, это всегда будет полезно.
-===========================================================================================================
- */
 
-    //-------------------------------  методы ----------------------------------------------------------
-    private Class<?> findClass(String SimpleName) {
-        String full = this.getClass().getName();
-        String dogPath = full.replace(this.getClass().getSimpleName(), SimpleName);
-        try {
-            return Class.forName(dogPath);
-        } catch (ClassNotFoundException e) {
-            fail("\nERROR:Тест не пройден. Класс " + SimpleName + " не найден.");
-        }
-        return null;
-    }
-
-    protected Method checkMethod(String className, String methodName, Class<?>... parameters) {
-        Class<?> aClass = this.findClass(className);
-        try {
-            methodName = methodName.trim();
-            Method m;
-            assert aClass != null;
-            if (methodName.startsWith("static")) {
-                methodName = methodName.replace("static", "").trim();
-                m = aClass.getDeclaredMethod(methodName, parameters);
-                if (!Modifier.isStatic(m.getModifiers())) {
-                    fail("\nERROR:Метод " + m.getName() + " должен быть статическим");
-                }
-            } else {
-                m = aClass.getDeclaredMethod(methodName, parameters);
-            }
-            m.setAccessible(true);
-            return m;
-
-        } catch (NoSuchMethodException e) {
-            System.err.println("\nERROR:Не найден метод " + methodName + " либо у него неверная сигнатура");
-            System.err.println("ERROR:Ожидаемый класс: " + className);
-            System.err.println("ERROR:Ожидаемый метод: " + methodName);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Method findMethod(Class<?> cl, String name, Class<?>... param) {
-        try {
-            return cl.getDeclaredMethod(name, param);
-        } catch (NoSuchMethodException e) {
-            fail("\nERROR:Тест не пройден. Метод " + cl.getName() + "." + name + " не найден\n");
-        }
-        return null;
-    }
-
-    private Object invoke(Method method, Object o, Object... value) {
-        try {
-            method.setAccessible(true);
-            return method.invoke(o, value);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            fail("\nERROR:Не удалось вызвать метод " + method.getName() + "\n");
-        }
-        return null;
-    }
-
-
-    //метод находит и создает класс для тестирования
-    //по имени вызывающего его метода, testTaskA1 будет работать с TaskA1
-    protected Test_jd01_14 run(String in) {
-        return run(in, true);
-    }
-
-    private Test_jd01_14 run(String in, boolean runMain) {
-        StackTraceElement element = Stream.of(new Throwable().getStackTrace())
-                .filter(e -> e.getMethodName().startsWith("test"))
-                .findFirst()
-                .orElse(null);
-        assert element != null;
-        String[] path = element.getClassName().split("\\.");
-        String nameTestMethod = element.getMethodName();
-        String clName = nameTestMethod.replace("test", "");
-        clName = clName.replaceFirst(".+__", "");
-        clName = element.getClassName().replace(path[path.length - 1], clName);
-        System.out.println("\n---------------------------------------------");
-        System.out.println("Старт теста для " + clName);
-        if (!in.isEmpty()) System.out.println("input:" + in);
-        System.out.println("---------------------------------------------");
-        return new Test_jd01_14(clName, in, runMain);
-    }
-
-    //-------------------------------  тест ----------------------------------------------------------
-    public Test_jd01_14() {
-    }
-
-    //переменные теста
-    public Class<?> aClass; //тестируемый класс
-    private final PrintStream oldOut = System.out; //исходный поток вывода
-    private final PrintStream newOut; //поле для перехвата потока вывода
-    private final StringWriter strOut = new StringWriter(); //накопитель строки вывода
-
-    //Основной конструктор тестов
-    private Test_jd01_14(String className, String in, boolean runMain) {
-        aClass = null;
-        try {
-            aClass = Class.forName(className);
-        } catch (ClassNotFoundException e) {
-            fail("ERROR:Не найден класс " + className + "\n");
-        }
-        InputStream reader = new ByteArrayInputStream(in.getBytes());
-        System.setIn(reader);   //перехват стандартного ввода
-        System.setOut(newOut);  //перехват стандартного вывода
-
-        if (runMain) //если нужно запускать, то запустим, иначе оставим только вывод
-            try {
-                Class<?>[] argTypes = new Class[]{String[].class};
-                Method main = aClass.getDeclaredMethod("main", argTypes);
-                main.invoke(null, (Object) new String[]{});
-                System.setOut(oldOut); //возврат вывода, нужен, только если был запуск
-            } catch (NoSuchMethodException e) {
-                fail("ERROR:В классе " + aClass.getSimpleName() + " нет метода \"public static void main\"");
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                fail("ERROR:В классе " + aClass.getSimpleName() + "не удалось выполнить метод \"public static void main\"");
-            }
-    }
-
-
-    //проверка вывода
-    private Test_jd01_14 is(String str) {
-        assertEquals("ERROR:Ожидается такой вывод:\n<---начало---->\n" + str + "<---конец--->",
-                strOut.toString(),
-                str);
-        return this;
-    }
-
-    public Test_jd01_14 include(String str) {
-        assertTrue("ERROR:Строка не найдена: " + str + "\n",
-                strOut.toString().contains(str));
-        return this;
-    }
-
-    private Test_jd01_14 exclude(String str) {
-        assertFalse("ERROR:Лишние данные в выводе: " + str + "\n",
-                strOut.toString().contains(str));
-        return this;
-    }
-
-    private Test_jd01_14 matches(String regexp) {
-        assertTrue("ERROR:вывод не соответствует паттерну: " + regexp + "\n",
-                strOut.toString().matches(regexp));
-        return this;
-    }
-
-    private Test_jd01_14 find(String regexp) {
-        Matcher matcher = Pattern.compile(regexp).matcher(strOut.toString());
-        assertTrue("ERROR:вывод не содержит паттерн: " + regexp + "\n",
-                matcher.find());
-        return this;
-    }
-
-
-    //логический блок перехвата вывода
-    {
-        newOut = new PrintStream(new OutputStream() {
-            private byte[] bytes = new byte[1];
-            private int pos = 0;
-
-            @Override
-            public void write(int b) {
-                if (pos == 0 && b == '\r') //пропуск \r (чтобы win mac и linux одинаково работали
-                    return;
-                if (pos == 0) { //определим кодировку https://ru.wikipedia.org/wiki/UTF-8
-                    if ((b & 0b11110000) == 0b11110000) bytes = new byte[4];
-                    else if ((b & 0b11100000) == 0b11100000) bytes = new byte[3];
-                    else if ((b & 0b11000000) == 0b11000000) bytes = new byte[2];
-                    else bytes = new byte[1];
-                }
-                bytes[pos++] = (byte) b;
-                if (pos == bytes.length) { //символ готов
-                    String s = new String(bytes); //соберем весь символ
-                    strOut.append(s); //запомним вывод для теста
-                    oldOut.append(s); //копию в обычный вывод
-                    pos = 0; //готовим новый символ
-                }
-
-            }
-        });
-    }
 }
