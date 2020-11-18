28177c040a10ecdb8ae842759016b4b97597f01a
Alexander Khmelov <akhmelev@gmail.com>
2020-11-19 00:25:21 +0300

    before create parent (by.it.HomeWork)

--- src/by/it/trukhanovich/jd01_08/Test_jd01_08.java
+++ src/by/it/trukhanovich/jd01_08/Test_jd01_08.java
@@ -1,6 +1,7 @@
 package by.it.trukhanovich.jd01_08;
 
 
+import by.it.HomeWork;
 import org.junit.Test;
 
 import java.io.*;
@@ -13,11 +14,11 @@ import static org.junit.Assert.*;
 @SuppressWarnings("all")
 
 //поставьте курсор на следующую строку и нажмите Ctrl+Shift+F10
-public class Test_jd01_08 {
+public class Test_jd01_08 extends HomeWork {
 
     @Test(timeout = 5000)
     public void testTaskA0_constructors__Scalar() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         assertEquals("У вас неверное наследование", "Var", ok.aClass.getSuperclass().getSimpleName());
 
         Constructor c = ok.aClass.getDeclaredConstructor(double.class);
@@ -59,7 +60,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskA1_add__Scalar() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(double.class);
         Object v1 = c.newInstance(1.23);
         Object v2 = c.newInstance(4.56);
@@ -76,7 +77,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskA2_sub__Scalar() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(double.class);
         Object v1 = c.newInstance(1.23);
         Object v2 = c.newInstance(4.56);
@@ -92,7 +93,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskA3_mul__Scalar() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(double.class);
         Object v1 = c.newInstance(1.23);
         Object v2 = c.newInstance(4.56);
@@ -108,7 +109,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskA4_div__Scalar() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(double.class);
         Object v1 = c.newInstance(1.23);
         Object v2 = c.newInstance(4.56);
@@ -125,7 +126,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskB0_constructors__Vector() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         assertEquals("У вас неверное наследование", "Var", ok.aClass.getSuperclass().getSimpleName());
         Constructor c = ok.aClass.getDeclaredConstructor(double[].class);
         System.out.println("Создание переменной типа Vector на основе массива {1,2,4}");
@@ -164,7 +165,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskB1_add__Vector() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(double[].class);
         Object v1 = c.newInstance(new double[]{1, 2, 3});
         Object v2 = c.newInstance(new double[]{4, 5, 6});
@@ -187,7 +188,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskB2_sub__Vector() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(double[].class);
         Object v1 = c.newInstance(new double[]{1, 2, 3});
         Object v2 = c.newInstance(new double[]{4, 5, 6});
@@ -210,7 +211,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskB3_mul__Vector() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(double[].class);
         Object v1 = c.newInstance(new double[]{1, 2, 3});
         Object v2 = c.newInstance(new double[]{4, 5, 6});
@@ -234,7 +235,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000, expected = Exception.class)
     public void testTaskB4_div__Vector() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(double[].class);
         Object v1 = c.newInstance(new double[]{1, 2, 3});
         Object v2 = c.newInstance(new double[]{4, 5, 6});
@@ -261,7 +262,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskC0_constructors__Matrix() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         assertEquals("У вас неверное наследование", "Var", ok.aClass.getSuperclass().getSimpleName());
         Constructor c = ok.aClass.getDeclaredConstructor(double[][].class);
         System.out.println("Создание переменной типа Matrix на основе массива {{1,2},{3,4}}");
@@ -300,7 +301,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskС1_add__Matrix() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(String.class);
         Object v1 = c.newInstance("{{1, 2}, {3, 4}}");
         Object v2 = c.newInstance("{{4, 5}, {7, 8}}");
@@ -323,7 +324,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskС2_sub__Matrix() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(String.class);
         Object v1 = c.newInstance("{{1, 2}, {3, 4}}");
         Object v2 = c.newInstance("{{4, 5}, {7, 8}}");
@@ -346,7 +347,7 @@ public class Test_jd01_08 {
 
     @Test(timeout = 5000)
     public void testTaskС3_mul__Matrix() throws Exception {
-        Test_jd01_08 ok = run("", false);
+        HomeWork ok = run("", false);
         Constructor c = ok.aClass.getDeclaredConstructor(String.class);
         Object v1 = c.newInstance("{{1, 2}, {3, 4}}");
         Object v2 = c.newInstance("{{4, 5}, {7, 8}}");
@@ -374,179 +375,5 @@ public class Test_jd01_08 {
         assertEquals("Операция со скаляром {{1, 2}, {3, 4}} * 2.0 работает некорректно",
                 "{{2.0, 4.0}, {6.0, 8.0}}", v3.toString());
     }
-    /*
-===========================================================================================================
-НИЖЕ ВСПОМОГАТЕЛЬНЫЙ КОД ТЕСТОВ. НЕ МЕНЯЙТЕ В ЭТОМ ФАЙЛЕ НИЧЕГО.
-Но изучить как он работает - можно, это всегда будет полезно.
-===========================================================================================================
- */
-    //-------------------------------  методы ----------------------------------------------------------
-    private Class findClass(String SimpleName) {
-        String full = this.getClass().getName();
-        String dogPath = full.replace(this.getClass().getSimpleName(), SimpleName);
-        try {
-            return Class.forName(dogPath);
-        } catch (ClassNotFoundException e) {
-            fail("\nERROR:Тест не пройден. Класс " + SimpleName + " не найден.");
-        }
-        return null;
-    }
 
-    private Method checkMethod(String className, String methodName, Class<?>... parameters) throws Exception {
-        Class aClass = this.findClass(className);
-        try {
-            methodName = methodName.trim();
-            Method m;
-            if (methodName.startsWith("static")) {
-                methodName = methodName.replace("static", "").trim();
-                m = aClass.getDeclaredMethod(methodName, parameters);
-                if ((m.getModifiers() & Modifier.STATIC) != Modifier.STATIC) {
-                    fail("\nERROR:Метод " + m.getName() + " должен быть статическим");
-                }
-            } else
-                m = aClass.getDeclaredMethod(methodName, parameters);
-            m.setAccessible(true);
-            return m;
-
-        } catch (NoSuchMethodException e) {
-            System.err.println("\nERROR:Не найден метод " + methodName + " либо у него неверная сигнатура");
-            System.err.println("ERROR:Ожидаемый класс: " + className);
-            System.err.println("ERROR:Ожидаемый метод: " + methodName);
-            return null;
-        }
-    }
-
-    private Method findMethod(Class<?> cl, String name, Class... param) {
-        try {
-            return cl.getDeclaredMethod(name, param);
-        } catch (NoSuchMethodException e) {
-            fail("\nERROR:Тест не пройден. Метод " + cl.getName() + "." + name + " не найден\n");
-        }
-        return null;
-    }
-
-    private Object invoke(Method method, Object o, Object... value) {
-        try {
-            method.setAccessible(true);
-            return method.invoke(o, value);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            fail("\nERROR:Не удалось вызвать метод " + method.getName() + "\n");
-        }
-        return null;
-    }
-
-
-    //метод находит и создает класс для тестирования
-    //по имени вызывающего его метода, testTaskA1 будет работать с TaskA1
-    private static Test_jd01_08 run(String in) {
-        return run(in, true);
-    }
-
-    private static Test_jd01_08 run(String in, boolean runMain) {
-        Throwable t = new Throwable();
-        StackTraceElement trace[] = t.getStackTrace();
-        StackTraceElement element;
-        int i = 0;
-        do {
-            element = trace[i++];
-        }
-        while (!element.getMethodName().contains("test"));
-
-        String[] path = element.getClassName().split("\\.");
-        String nameTestMethod = element.getMethodName();
-        String clName = nameTestMethod.replace("test", "");
-        clName = clName.replaceFirst(".+__", "");
-        clName = element.getClassName().replace(path[path.length - 1], clName);
-        System.out.println("\n---------------------------------------------");
-        System.out.println("Старт теста для " + clName);
-        if (!in.isEmpty()) System.out.println("input:" + in);
-        System.out.println("---------------------------------------------");
-        return new Test_jd01_08(clName, in, runMain);
-    }
-
-    //-------------------------------  тест ----------------------------------------------------------
-    public Test_jd01_08() {
-        //Конструктор тестов
-    }
-
-    //переменные теста
-    private String className;
-    private Class<?> aClass;
-    private PrintStream oldOut = System.out; //исходный поток вывода
-    private PrintStream newOut; //поле для перехвата потока вывода
-    private StringWriter strOut = new StringWriter(); //накопитель строки вывода
-
-    //Основной конструктор тестов
-    private Test_jd01_08(String className, String in, boolean runMain) {
-        //this.className = className;
-        aClass = null;
-        try {
-            aClass = Class.forName(className);
-            this.className = className;
-
-        } catch (ClassNotFoundException e) {
-            fail("ERROR:Не найден класс " + className + "\n");
-        }
-        InputStream reader = new ByteArrayInputStream(in.getBytes());
-        System.setIn(reader);   //перехват стандартного ввода
-        System.setOut(newOut);  //перехват стандартного вывода
-
-        if (runMain) //если нужно запускать, то запустим, иначе оставим только вывод
-            try {
-                Class[] argTypes = new Class[]{String[].class};
-                Method main = aClass.getDeclaredMethod("main", argTypes);
-                main.invoke(null, (Object) new String[]{});
-                System.setOut(oldOut); //возврат вывода, нужен, только если был запуск
-            } catch (Exception x) {
-                x.printStackTrace();
-            }
-    }
-
-
-    //проверка вывода
-    private Test_jd01_08 is(String str) {
-        assertTrue("ERROR:Ожидается такой вывод:\n<---начало---->\n" + str + "<---конец--->",
-                strOut.toString().equals(str));
-        return this;
-    }
-
-    private Test_jd01_08 include(String str) {
-        assertTrue("ERROR:Строка не найдена: " + str + "\n", strOut.toString().contains(str));
-        return this;
-    }
-
-    private Test_jd01_08 exclude(String str) {
-        assertTrue("ERROR:Лишние данные в выводе: " + str + "\n", !strOut.toString().contains(str));
-        return this;
-    }
-
-
-    //логический блок перехвата вывода
-    {
-        newOut = new PrintStream(new OutputStream() {
-            private byte bytes[] = new byte[1];
-            private int pos = 0;
-
-            @Override
-            public void write(int b) throws IOException {
-                if (pos == 0 && b == '\r') //пропуск \r (чтобы win mac и linux одинаково работали
-                    return;
-                //ок. теперь можно делать разбор
-                if (pos == 0) { //определим кодировку https://ru.wikipedia.org/wiki/UTF-8
-                    if ((b & 0b11110000) == 0b11110000) bytes = new byte[4];
-                    else if ((b & 0b11100000) == 0b11100000) bytes = new byte[3];
-                    else if ((b & 0b11000000) == 0b11000000) bytes = new byte[2];
-                    else bytes = new byte[1];
-                }
-                bytes[pos++] = (byte) b;
-                if (pos == bytes.length) { //символ готов
-                    String s = new String(bytes); //соберем весь символ
-                    strOut.append(s); //запомним вывод для теста
-                    oldOut.append(s); //копию в обычный вывод
-                    pos = 0; //готовим новый символ
-                }
-            }
-        });
-    }
 }
--- src/by/it/trukhanovich/jd01_13/TaskC.java
+++ src/by/it/trukhanovich/jd01_13/TaskC.java
@@ -4,31 +4,39 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TaskC {
-    private static ArrayList arrayList=new ArrayList();
+    private static final ArrayList<Double> arrayList = new ArrayList<>();
     private static Scanner sc;
-    static int i=0;
-    public static void main(String[] args) throws InterruptedException {
-            TaskC.sc = new Scanner(System.in);
-            readData();
-    }
-    static void readData() throws NumberFormatException, InterruptedException {
-        for(;;) {
-            try {
-                String s = TaskC.sc.nextLine();
-                if (s.equals("end")) return;
-                double v = Double.parseDouble(s);
-                TaskC.arrayList.add(v);
-            } catch (NumberFormatException e) {
-                Thread.sleep(100);
-                if (i < 4) {
-                    for (int i1 = arrayList.size() - 1; i1 >= 0; i1--) {
-                        System.out.print(arrayList.get(i1) + " ");
-                    }
-                    i++;
-                    readData();
-                } else throw new RuntimeException(e);
-            }
+    static int i = 0;
 
+    public static void main(String[] args) {
+        TaskC.sc = new Scanner(System.in);
+        for (int j = 0; j < 10000; j++) {
+            readData();
+        }
+    }
+
+    static void readData() {
+        try {
+            double v = Double.parseDouble(TaskC.sc.next());
+            TaskC.arrayList.add(v);
+        } catch (RuntimeException e) {
+            if (i < 5) {
+                sleep();
+                for (int j = arrayList.size() - 1; j >= 0; j--) {
+                    System.out.print(arrayList.get(j) + " ");
+                }
+                System.out.println();
+                i++;
+            } else
+                throw e;
+        }
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException interruptedException) {
+            interruptedException.printStackTrace();
         }
     }
 }
